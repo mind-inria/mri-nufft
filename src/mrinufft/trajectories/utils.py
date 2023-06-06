192d727a0022c2449c78a626cfa1d3a2eaa9190e
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-06-06 11:42:54 -0400

    docs(trajectories): add docstring for utils.
    Thanks you copilot.

--- src/mrinufft/trajectories/utils.py
+++ src/mrinufft/trajectories/utils.py
@@ -1,3 +1,4 @@
+"""Utility functions for the trajectory design."""
 import numpy as np
 
 
@@ -21,20 +22,55 @@ DEFAULT_SMAX = 100.0  # T/m/s
 # CONSTRAINTS #
 ###############
 
-def compute_gradients(trajectory,
-                      resolution=DEFAULT_RESOLUTION,
-                      raster_time=DEFAULT_RASTER_TIME,
-                      g_ratio=DEFAULT_GYROMAGNETIC_RATIO):
+
+def compute_gradients(
+    trajectory,
+    resolution=DEFAULT_RESOLUTION,
+    raster_time=DEFAULT_RASTER_TIME,
+    g_ratio=DEFAULT_GYROMAGNETIC_RATIO,
+):
+    """Compute Gradient and Slew rate from a trajectory."""
     trajectory = trajectory / KMAX / (2 * resolution * g_ratio)
     gradients = np.diff(trajectory, axis=1) / raster_time
     slews = np.diff(gradients, axis=1) / raster_time
     return gradients, slews
 
-def check_gradient_constraints(trajectory,
-                               resolution=DEFAULT_RESOLUTION,
-                               raster_time=DEFAULT_RASTER_TIME,
-                               g_ratio=DEFAULT_GYROMAGNETIC_RATIO,
-                               gmax=DEFAULT_GMAX, smax=DEFAULT_SMAX):
+
+def check_gradient_constraints(
+    trajectory,
+    resolution=DEFAULT_RESOLUTION,
+    raster_time=DEFAULT_RASTER_TIME,
+    g_ratio=DEFAULT_GYROMAGNETIC_RATIO,
+    gmax=DEFAULT_GMAX,
+    smax=DEFAULT_SMAX,
+):
+    """Check if a trajectory satisfies the gradient constraints.
+
+    Parameters
+    ----------
+    trajectory : np.ndarray
+        Trajectory to check.
+    resolution : float, optional
+        Resolution of the trajectory in m. The default is DEFAULT_RESOLUTION.
+    raster_time : float, optional
+        Raster time of the trajectory in s. The default is DEFAULT_RASTER_TIME.
+    g_ratio : float, optional
+        Gyromagnetic ratio of the trajectory in Hz/T.
+        The default is DEFAULT_GYROMAGNETIC_RATIO.
+    gmax : float, optional
+        Maximum gradient amplitude in T/m. The default is DEFAULT_GMAX.
+    smax : float, optional
+        Maximum slew rate in T/m/s. The default is DEFAULT_SMAX.
+
+    Returns
+    -------
+    bool
+        True if the trajectory satisfies the constraints, False otherwise.
+    float
+        Maximum gradient amplitude in T/m.
+    float
+        Maximum slew rate in T/m/s.
+    """
     gradients, slews = compute_gradients(trajectory, resolution, raster_time, g_ratio)
     max_grad = np.max(np.linalg.norm(gradients, axis=-1))
     max_slew = np.max(np.linalg.norm(slews, axis=-1))
@@ -45,12 +81,46 @@ def check_gradient_constraints(trajectory,
 # MATHEMATICS #
 ###############
 
+
 def compute_greatest_common_divider(p, q):
+    """Compute the greatest common divider of two integers p and q.
+
+    Parameters
+    ----------
+    p : int
+        First integer.
+    q : int
+        Second integer.
+
+    Returns
+    -------
+    int
+        The greatest common divider of p and q.
+    """
     while q != 0:
         p, q = q, p % q
     return p
 
+
 def compute_coprime_factors(Nc, length, start=1, update=1):
+    """Compute a list of coprime factors of Nc.
+
+    Parameters
+    ----------
+    Nc : int
+        Number to factorize.
+    length : int
+        Number of coprime factors to compute.
+    start : int, optional
+        First number to check. The default is 1.
+    update : int, optional
+        Increment between two numbers to check. The default is 1.
+
+    Returns
+    -------
+    list
+        List of coprime factors of Nc.
+    """
     count = start
     coprimes = []
     while len(coprimes) < length:
@@ -64,32 +134,106 @@ def compute_coprime_factors(Nc, length, start=1, update=1):
 # ROTATIONS #
 #############
 
-# Initialize 2D rotation matrix
+
 def R2D(theta):
-  return np.array([[np.cos(theta), -np.sin(theta)],
-                   [np.sin(theta),  np.cos(theta)]])
+    """Initialize 2D rotation matrix.
+
+    Parameters
+    ----------
+    theta : float
+        Rotation angle in rad.
+
+    Returns
+    -------
+    np.ndarray
+        2D rotation matrix.
+    """
+    return np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
 
 
-# Initialize 3D rotation matrix around x axis
 def Rx(theta):
-  return np.array([[1,             0,              0],
-                   [0, np.cos(theta), -np.sin(theta)],
-                   [0, np.sin(theta),  np.cos(theta)]])
+    """Initialize 3D rotation matrix around x axis.
 
-# Initialize 3D rotation matrix around y axis
+    Parameters
+    ----------
+    theta : float
+        Rotation angle in rad.
+
+    Returns
+    -------
+    np.ndarray
+        2D rotation matrix.
+    """
+    return np.array(
+        [
+            [1, 0, 0],
+            [0, np.cos(theta), -np.sin(theta)],
+            [0, np.sin(theta), np.cos(theta)],
+        ]
+    )
+
+
 def Ry(theta):
-  return np.array([[ np.cos(theta), 0, np.sin(theta)],
-                   [             0, 1,             0],
-                   [-np.sin(theta), 0, np.cos(theta)]])
+    """Initialize 3D rotation matrix around y axis.
 
-# Initialize 3D rotation matrix around z axis
+    Parameters
+    ----------
+    theta : float
+        Rotation angle in rad.
+
+    Returns
+    -------
+    np.ndarray
+        2D rotation matrix.
+    """
+    return np.array(
+        [
+            [np.cos(theta), 0, np.sin(theta)],
+            [0, 1, 0],
+            [-np.sin(theta), 0, np.cos(theta)],
+        ]
+    )
+
+
 def Rz(theta):
-  return np.array([[np.cos(theta), -np.sin(theta), 0],
-                   [np.sin(theta),  np.cos(theta), 0],
-                   [            0,              0, 1]])
+    """Initialize 3D rotation matrix around z axis.
+
+    Parameters
+    ----------
+    theta : float
+        Rotation angle in rad.
+
+    Returns
+    -------
+    np.ndarray
+        2D rotation matrix.
+    """
+    return np.array(
+        [
+            [np.cos(theta), -np.sin(theta), 0],
+            [np.sin(theta), np.cos(theta), 0],
+            [0, 0, 1],
+        ]
+    )
 
 
 def Rv(v1, v2, normalize=True):
+    """Initialize 3D rotation matrix from two vectors.
+
+    Parameters
+    ----------
+    v1 : np.ndarray
+        First vector.
+    v2 : np.ndarray
+        Second vector.
+    normalize : bool, optional
+        Normalize the vectors. The default is True.
+
+    Returns
+    -------
+    np.ndarray
+        3D rotation matrix.
+    """
     if normalize:
         v1, v2 = v1 / np.linalg.norm(v1), v2 / np.linalg.norm(v2)
     cos_theta = np.dot(v1, v2)
@@ -104,6 +248,38 @@ def Rv(v1, v2, normalize=True):
 
 
 def initialize_tilt(tilt, nb_partitions=1):
+    r"""Initialize the tilt angle.
+
+    Parameters
+    ----------
+    tilt : str or float
+        Tilt angle in rad or name of the tilt.
+    nb_partitions : int, optional
+        Number of partitions. The default is 1.
+
+    Returns
+    -------
+    float
+        Tilt angle in rad.
+
+    Raises
+    ------
+    NotImplementedError
+        If the tilt name is unknown.
+
+    Notes
+    -----
+    The following values are accepted for the tilt name, with :math:`N` the number of
+    partitions:
+
+    - "none": no tilt
+    - "uniform": uniform tilt: 2:math:`\pi / N`
+    - "intergaps": :math:`\pi/2N`
+    - "inverted": inverted tilt :math:`\pi/N + \pi`
+    - "golden": tilt of the golden angle :math:`\pi(3-\sqrt{5})`
+    - "mri golden": tilt of the golden angle used in MRI :math:`\pi(\sqrt{5}-1)/2`
+
+    """
     if not isinstance(tilt, str):
         return tilt
     elif tilt == "none":
