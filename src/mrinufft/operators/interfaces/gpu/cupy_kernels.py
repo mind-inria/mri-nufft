f194a9c80eb1798e17bd4d8c41435146d17ac42c
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-07-03 16:52:51 +0200

    feat: detect MaxThreadPerBlock for GPU.

--- src/mrinufft/operators/interfaces/gpu/cupy_kernels.py
+++ src/mrinufft/operators/interfaces/gpu/cupy_kernels.py
@@ -10,14 +10,24 @@ except ImportError:
 update_density_kernel = lambda *args, **kwargs: None  # noqa: E731
 sense_adj_mono = lambda *args, **kwargs: None  # noqa: E731
 
+
+def _get_block_size():
+    """Get the warp size of the current device."""
+    if CUPY_AVAILABLE:
+        device = cp.cuda.runtime.getDevice()
+        return cp.cuda.runtime.getDeviceProoperties(device)["MaxThreadsPerBlock"]
+    raise RuntimeError("Cupy is not available")
+
+
 if CUPY_AVAILABLE:
     update_density_kernel = cp.RawKernel(
         """
         extern "C" __global__
-        void update_density_kernel(float2* density, const float2* update)
+        void update_density_kernel(float2* density, const float2* update, const unsigned long len)
         {
-          int t = blockDim.x * blockIdx.x + threadIdx.x;
-          density[t].x *= rsqrtf(update[t].x *update[t].x + update[t].y * update[t].y);
+          unsigned long t = blockDim.x * blockIdx.x + threadIdx.x;
+          if(t < len)
+            density[t].x *= rsqrtf(update[t].x *update[t].x + update[t].y * update[t].y);
         }
         """,
         "update_density_kernel",
@@ -26,11 +36,14 @@ if CUPY_AVAILABLE:
     sense_adj_mono_kernel = cp.RawKernel(
         """
         extern "C" __global__
-        void sense_adj_mono_kernel(float2* dest, const float2* img, const float2* smap)
+        void sense_adj_mono_kernel(float2* dest, const float2* img, const float2* smap, const unsigned long len)
         {
-          int t = blockDim.x * blockIdx.x + threadIdx.x;
-          dest[t].x += img[t].x * smap[t].x + img[t].y * smap[t].y;
-          dest[t].y += img[t].y * smap[t].x - img[t].x * smap[t].y;
+          unsigned long t = blockDim.x * blockIdx.x + threadIdx.x;
+          if (t < len)
+          {
+            dest[t].x += img[t].x * smap[t].x + img[t].y * smap[t].y;
+            dest[t].y += img[t].y * smap[t].x - img[t].x * smap[t].y;
+          }
         }
         """,
         "sense_adj_mono_kernel",
@@ -49,7 +62,13 @@ def update_density(density, update):
     -----
     ``density[i] /= sqrt(abs(update[i]))``
     """
-    update_density_kernel((len(density) // 1024,), (1024,), (density, update))
+    block_size = _get_block_size()
+    update_density_kernel(
+        ((len(density) // block_size) + 1,),
+        (block_size,),
+        (density, update),
+        len(density),
+    )
 
 
 def sense_adj_mono(dest, coil, smap, **kwargs):
@@ -64,4 +83,11 @@ def sense_adj_mono(dest, coil, smap, **kwargs):
     smap: GPUArray
         The sensitivity profile of the coil.
     """
-    sense_adj_mono_kernel((dest.size // 1024,), (1024,), (dest, coil, smap), **kwargs)
+    block_size = _get_block_size()
+    sense_adj_mono_kernel(
+        (dest.size // block_size,),
+        (block_size,),
+        (dest, coil, smap),
+        dest.size,
+        **kwargs,
+    )
