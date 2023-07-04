6999c8d5c1367757c29f3ab2aa27b64af0040cb4
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-07-04 15:08:34 +0200

    fix: missing attribute.

--- src/mrinufft/operators/interfaces/cpu/finufft_interface.py
+++ src/mrinufft/operators/interfaces/cpu/finufft_interface.py
@@ -93,7 +93,7 @@ class MRIfinufft(AbstractMRIcpuNUFFT):
         shape,
         density=False,
         n_coils=1,
-        n_batches=1,
+        n_batchs=1,
         n_trans=1,
         smaps=None,
         keep_dims=False,
@@ -134,7 +134,7 @@ class MRIfinufft(AbstractMRIcpuNUFFT):
             self._smaps = smaps
         else:
             self._uses_sense = False
-
+        self.n_batchs = n_batchs
         # Initialise NUFFT plans
         self.raw_op = RawFinufftPlan(
             self.samples,
