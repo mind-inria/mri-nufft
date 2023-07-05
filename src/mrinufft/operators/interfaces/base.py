fix: bugs with trajectory normalization.

src/mrinufft/operators/interfaces/base.py
@@ -28,7 +28,7 @@ def proper_trajectory(trajectory, normalize=True):
     """
     # flatten to a list of point
     try:
-        new_traj = np.asarray(trajectory)
+        new_traj = np.asarray(trajectory).copy()
     except Exception as e:
         raise ValueError(
             "trajectory should be array_like, with the last dimension being coordinates"

src/mrinufft/operators/interfaces/cpu/finufft_interface.py
@@ -102,20 +102,19 @@ class MRIfinufft(AbstractMRIcpuNUFFT):
         if not FINUFFT_AVAILABLE:
             raise RuntimeError("finufft is not available.")
         self.shape = shape
-        self.n_samples = len(samples)
 
         # we will access the samples by their coordinate first.
         self.samples = proper_trajectory(np.asfortranarray(samples), normalize=True)
-
+        self.n_samples = len(self.samples)
         self._dtype = self.samples.dtype
         self._cpx_dtype = np.complex128 if self._dtype == "float64" else np.complex64
         self._uses_sense = False
 
         # Density Compensation Setup
         if density is True:
-            self.density = self.estimate_density(samples, shape)
+            self.density = self.estimate_density(self.samples, shape)
         elif isinstance(density, np.ndarray):
-            if len(density) != len(samples):
+            if len(density) != len(self.samples):
                 raise ValueError(
                     "Density array and samples array should have the same length."
                 )
@@ -173,8 +172,8 @@ class MRIfinufft(AbstractMRIcpuNUFFT):
             ret = self._op_calibless(data, ksp)
         if self.keep_dims:
             return ret
-        else:
-            return ret.squeeze(axis=(0, 0))
+        else:  # squeeze the batch and coil dimensions.
+            return ret.squeeze(axis=(0, 1))
 
     def _op_sense(self, data, ksp_d=None):
         if self.n_batchs > 1:
@@ -222,7 +221,7 @@ class MRIfinufft(AbstractMRIcpuNUFFT):
         if self.keep_dims:
             return ret
         else:
-            return ret.squeeze(axis=(0, 0))
+            return ret.squeeze(axis=(0, 1))
 
     def _adj_op_sense(self, coeffs, img=None):
         if self.n_batchs > 1:
@@ -248,7 +247,7 @@ class MRIfinufft(AbstractMRIcpuNUFFT):
     @property
     def norm_factor(self):
         """Norm factor of the operator."""
-        return np.sqrt(np.prod(self.shape) * 2 ** len(self.shape))
+        return np.sqrt(np.prod(self.shape) * (2 ** len(self.shape)))
 
     @classmethod
     def estimate_density(cls, samples, shape, n_iter=1, **kwargs):

src/mrinufft/trajectories/density.py
@@ -69,7 +69,6 @@ def _voronoi(kspace):
     wi: array_like
         array of shape (M,) containing the density compensation weights.
     """
-    kspace = proper_trajectory(kspace)
     M = kspace.shape[0]
     if kspace.shape[1] == 2:
         vol = vol2d
@@ -104,6 +103,7 @@ def voronoi(kspace):
         array of shape (M, 2) or (M, 3) containing the coordinates of the points.
     """
     # deduplication only works for the 0,0 coordinate !!
+    kspace = proper_trajectory(kspace)
     i0 = np.sum(np.abs(kspace), axis=1) == 0
     if np.any(i0):
         i0f = np.where(i0)