9c0bccdcb3de29c6306a53c2195223367154ef01
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-07-17 11:41:57 +0200

    add backend for the density test.

--- tests/test_density.py
+++ tests/test_density.py
@@ -1,10 +1,8 @@
 """Test the density compensation estimations."""
-import numpy as np
 import numpy.testing as npt
-from pytest_cases import parametrize_with_cases, parametrize, fixture
-import pytest
+from pytest_cases import parametrize_with_cases, parametrize
 
-from mrinufft.operators.interfaces.cufinufft import CUFINUFFT_AVAILABLE, pipe
+from mrinufft.operators.interfaces.cufinufft import pipe
 from case_trajectories import CasesTrajectories
 
 
@@ -12,8 +10,8 @@ from case_trajectories import CasesTrajectories
     "kspace_traj, shape",
     cases=[CasesTrajectories.case_radial3D],
 )
-@pytest.mark.skipif(not CUFINUFFT_AVAILABLE, reason="cufinufft not yet implemented")
-def test_density_pipe(kspace_traj, shape):
+@parametrize("backend", ["cufinufft"])
+def test_density_pipe(kspace_traj, shape, backend):
     """Test the density compensation estimations."""
     density = pipe(kspace_traj, shape, num_iter=20, tol=2e-7).get()
     density_ref = pipe(kspace_traj, shape, num_iter=25, tol=2e-7).get()
