6618bf660c61b50d75c9d996dbd5f2ac604e9818
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-07-18 16:18:34 +0200

    test the batch computation with sense too.

--- tests/test_batch.py
+++ tests/test_batch.py
@@ -13,30 +13,53 @@ from case_trajectories import CasesTrajectories
 
 @fixture(scope="module")
 @parametrize(
-    "n_coils, n_batch, n_trans",
-    [(1, 1, 1), (1, 2, 1), (2, 1, 1), (2, 1, 2), (2, 2, 1), (2, 2, 2)],
+    "n_batch, n_coils, n_trans, sense",
+    [
+        (1, 1, 1, False),
+        (3, 1, 1, False),
+        (1, 4, 1, False),
+        (1, 4, 1, True),
+        (1, 4, 2, False),
+        (3, 2, 1, False),
+        (3, 2, 1, True),
+        (3, 4, 2, True),
+    ],
 )
 @parametrize_with_cases("kspace_locs, shape", cases=CasesTrajectories)
 @parametrize(backend=["finufft", "cufinufft"])
 def operator(
-    request, kspace_locs, shape, n_coils=1, n_batch=1, n_trans=1, backend="finufft"
+    request,
+    kspace_locs,
+    shape,
+    n_coils=1,
+    sense=None,
+    n_batch=1,
+    n_trans=1,
+    backend="finufft",
 ):
     """Generate a batch operator."""
-    return get_operator(backend)(kspace_locs, shape, n_coils=n_coils, smaps=None)
+    if sense:
+        smaps = np.random.rand(n_coils, *shape).astype(np.complex64)
+    else:
+        smaps = None
+    return get_operator(backend)(kspace_locs, shape, n_coils=n_coils, smaps=smaps)
 
 
 @fixture(scope="module")
 def flat_operator(operator):
     """Generate a batch operator with n_batch=1."""
     return get_operator(operator.backend)(
-        operator.samples, operator.shape, n_coils=operator.n_coils
+        operator.samples, operator.shape, n_coils=operator.n_coils, smaps=operator.smaps
     )
 
 
 @fixture(scope="module")
 def image_data(operator):
     """Generate a random image."""
-    shape = (operator.n_batchs, operator.n_coils, *operator.shape)
+    if operator.uses_sense:
+        shape = (operator.n_batchs, *operator.shape)
+    else:
+        shape = (operator.n_batchs, operator.n_coils, *operator.shape)
     img = np.random.rand(*shape).astype(operator.cpx_dtype)
     img = 1j * np.random.rand(*shape).astype(operator.cpx_dtype)
     return img
@@ -52,10 +75,13 @@ def kspace_data(operator):
 
 
 def test_batch_type2(operator, flat_operator, image_data):
-    """Test the batch type 1."""
+    """Test the batch type 2 (forward)."""
     kspace_data = operator.op(image_data)
 
-    image_flat = image_data.reshape(-1, operator.n_coils, *operator.shape)
+    if operator.uses_sense:
+        image_flat = image_data.reshape(-1, *operator.shape)
+    else:
+        image_flat = image_data.reshape(-1, operator.n_coils, *operator.shape)
     kspace_flat = [None] * operator.n_batchs
     for i in range(len(kspace_flat)):
         kspace_flat[i] = flat_operator.op(image_flat[i])
@@ -69,7 +95,7 @@ def test_batch_type2(operator, flat_operator, image_data):
 
 
 def test_batch_type1(operator, flat_operator, kspace_data):
-    """Test the batch type 1."""
+    """Test the batch type 1 (adjoint)."""
     image_data = operator.adj_op(kspace_data)
 
     kspace_flat = kspace_data.reshape(-1, operator.n_coils, operator.n_samples)
@@ -77,9 +103,13 @@ def test_batch_type1(operator, flat_operator, kspace_data):
     for i in range(len(image_flat)):
         image_flat[i] = flat_operator.adj_op(kspace_flat[i])
 
+    if not operator.uses_sense:
+        shape = (operator.n_batchs, operator.n_coils, *operator.shape)
+    else:
+        shape = (operator.n_batchs, *operator.shape)
     image_flat = np.reshape(
         np.concatenate(image_flat, axis=0),
-        (operator.n_batchs, operator.n_coils, *operator.shape),
+        shape,
     )
 
     npt.assert_allclose(image_data, image_flat, atol=1e-5, rtol=1e-4)
