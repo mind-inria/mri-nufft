497fabd92c18af7a171fb752b0c948f4e29351bf
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-07-26 14:54:20 +0200

    refactor: cleanup for gpunufft.

--- src/mrinufft/operators/interfaces/gpunufft.py
+++ src/mrinufft/operators/interfaces/gpunufft.py
@@ -10,136 +10,6 @@ except ImportError:
     GPUNUFFT_AVAILABLE = False
 
 
-class RawGpuNUFFT:
-    """GPU implementation of N-D non uniform Fast Fourrier Transform class.
-
-    Original implementation in PySAP-MRI: https://github.com/CEA-COSMIC/pysap-mri
-    """
-
-    def __init__(
-        self,
-        samples,
-        shape,
-        n_coils=1,
-        density_comp=None,
-        kernel_width=3,
-        sector_width=8,
-        osf=2,
-        balance_workload=True,
-        smaps=None,
-    ):
-        """Initilize the 'NUFFT' class.
-
-        Parameters
-        ----------
-        samples: np.ndarray
-            the kspace sample locations in the Fourier domain,
-            normalized between -0.5 and 0.5
-        shape: tuple of int
-            shape of the image
-        n_coils: int
-            Number of coils used to acquire the signal in case of multiarray
-            receiver coils acquisition
-        density_comp: np.ndarray default None.
-            k-space weighting, density compensation, if not specified
-            equal weightage is given.
-        kernel_width: int default 3
-            interpolation kernel width (usually 3 to 7)
-        sector_width: int default 8
-            sector width to use
-        osf: int default 2
-            oversampling factor (usually between 1 and 2)
-        balance_workload: bool default True
-            whether the workloads need to be balanced
-        smaps: np.ndarray default None
-            Holds the sensitivity maps for SENSE reconstruction
-        """
-        self.n_coils = n_coils
-        self.shape = shape
-
-        self.samples = proper_trajectory(samples, normalize="unit")
-
-        if density_comp is None:
-            density_comp = np.ones(samples.shape[0])
-        if smaps is None:
-            self.uses_sense = False
-        else:
-            smaps = np.asarray(
-                [np.reshape(smap_ch.T, smap_ch.size) for smap_ch in smaps]
-            ).T
-            self.uses_sense = True
-
-        self.operator = NUFFTOp(
-            np.reshape(samples, samples.shape[::-1], order="F"),
-            shape,
-            n_coils,
-            smaps,
-            density_comp,
-            kernel_width,
-            sector_width,
-            osf,
-            balance_workload,
-        )
-
-    def op(self, image, interpolate_data=False):
-        """This method calculates the masked non-cartesian Fourier transform
-        of a 2D / 3D image.
-
-        Parameters
-        ----------
-        image: np.ndarray
-            input array with the same shape as shape.
-        interpolate_data: bool, default False
-            if set to True, the image is just apodized and interpolated to
-            kspace locations. This is used for density estimation.
-
-        Returns
-        -------
-        np.ndarray
-            Non Uniform Fourier transform of the input image.
-        """
-        # Base gpuNUFFT Operator is written in CUDA and C++, we need to
-        # reorganize data to follow a different memory hierarchy
-        # TODO we need to update codes to use np.reshape for all this directly
-        if self.n_coils > 1 and not self.uses_sense:
-            coeff = self.operator.op(
-                np.asarray(
-                    [np.reshape(image_ch.T, image_ch.size) for image_ch in image]
-                ).T,
-                interpolate_data,
-            )
-        else:
-            coeff = self.operator.op(np.reshape(image.T, image.size), interpolate_data)
-            # Data is always returned as num_channels X coeff_array,
-            # so for single channel, we extract single array
-        return coeff
-
-    def adj_op(self, coeff, grid_data=False):
-        """This method calculates adjoint of non-uniform Fourier
-        transform of a 1-D coefficients array.
-
-        Parameters
-        ----------
-        coeff: np.ndarray
-            masked non-uniform Fourier transform 1D data.
-        grid_data: bool, default False
-            if True, the kspace data is gridded and returned,
-            this is used for density compensation
-        Returns
-        -------
-        np.ndarray
-            adjoint operator of Non Uniform Fourier transform of the
-            input coefficients.
-        """
-        image = self.operator.adj_op(coeff, grid_data)
-        print("adj_op ok", image.shape, flush=True)
-        if self.uses_sense:
-            image = image[None, ...]
-        trans_image = np.transpose(image, axes=(0, *range(1, 3 + 1)[::-1]))
-        dimension = self.samples.shape[-1]
-        return trans_image if dimension == 3 else trans_image[..., 0]
-
-
 class gpuNUFFT:
     """GPU implementation of N-D non uniform Fast Fourrier Transform class.
 
@@ -295,7 +165,7 @@ class MRIGpuNUFFT(FourierOperatorBase):
 
     backend = "gpunufft"
 
-    def __init__(self, samples, shape, n_coils=1, density=None, **kwargs):
+    def __init__(self, samples, shape, n_coils=1, density=None, smaps=None, **kwargs):
         """Initialize the class.
 
         Parameters
@@ -324,7 +194,9 @@ class MRIGpuNUFFT(FourierOperatorBase):
             )
         self.shape = shape
         self.samples = proper_trajectory(samples, normalize="unit")
+        self.dtype = self.samples.dtype
         self.n_coils = n_coils
+        self.smaps = smaps
         if density is True:
             self.density = pipe(self.samples, shape)
         elif isinstance(density, np.ndarray):
@@ -337,6 +209,7 @@ class MRIGpuNUFFT(FourierOperatorBase):
             shape=self.shape,
             n_coils=self.n_coils,
             density_comp=self.density,
+            smaps=smaps,
             **self.kwargs
         )
 
@@ -369,6 +242,9 @@ class MRIGpuNUFFT(FourierOperatorBase):
             inverse discrete Fourier transform of the input coefficients.
         """
         return self.impl.adj_op(coeffs, *args)
+
+    def data_consistency(self, data, obs_data):
+        return self.adj_op(self.op(data) - obs_data)
 
     @property
     def uses_sense(self):
