336728aac98ad422a7587ef6134a51ad685af727
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-08-29 10:55:58 +0200

    feat(test): add a backend argument to filter tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,13 +4,29 @@ import pytest
 from mrinufft.operators import check_backend
 
 
+def pytest_addoption(parser):
+    """Add option to pytest."""
+    parser.addoption(
+        "--backend",
+        action="append",
+        default=[],
+        help="list of backend to test. Unavailable backend will be skipped.",
+    )
+
+
+def isrunnable(backend, context):
+    """Check if test is runnable with the backend and its context."""
+    backend_args = context.config.getoption("backend")
+    return check_backend(backend) and (backend_args and backend in backend_args)
+
+
 # # for test directly parametrized by a backend
 def pytest_runtest_setup(item):
     """Skip tests based on the backend."""
     if hasattr(item, "callspec"):
         if "backend" in item.callspec.params:
             backend = item.callspec.params["backend"]
-            if not check_backend(backend):
+            if not isrunnable(backend, item):
                 pytest.skip(f"Backend {backend} not available.")
 
 
@@ -31,7 +47,7 @@ def pytest_generate_tests(metafunc):
                 ][0]
             ]
             # Only keep the callspec if the backend is available.
-            if not check_backend(backend):
+            if not isrunnable(backend, metafunc):
                 callspec.marks.append(
                     pytest.mark.skip(f"Backend {backend} not available.")
                 )
