c6befc62febcab6e11372fa85503cd4b7f5ec271
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-08-31 11:11:15 +0200

    feat(pykeops): make pykeops run the nudft.

--- src/mrinufft/operators/interfaces/pykeops.py
+++ src/mrinufft/operators/interfaces/pykeops.py
@@ -1,4 +1,7 @@
-"""Non Uniform Fourier transform with PyKeOps and torch."""
+"""Non Uniform Fourier transform with PyKeOps and torch.
+
+TODO: Use Keops for the non fourier stuff (e.g. smaps and multi coil )
+"""
 
 import numpy as np
 from .base import FourierOperatorCPU, proper_trajectory
@@ -6,6 +9,7 @@ from .base import FourierOperatorCPU, proper_trajectory
 PYKEOPS_AVAILABLE = True
 try:
     from pykeops.numpy import ComplexLazyTensor as NumpyComplexLazyTensor
+    from pykeops.numpy import LazyTensor as NumpyLazyTensor
     from pykeops.numpy import Genred as NumpyGenred
 except ImportError:
     PYKEOPS_AVAILABLE = False
@@ -18,6 +22,7 @@ except ImportError:
 
 if PYTORCH_AVAILABLE:
     from pykeops.torch import ComplexLazyTensor as TorchComplexLazyTensor
+    from pykeops.torch import LazyTensor as TorchLazyTensor
     from pykeops.torch import Genred as TorchGenred
 
 
@@ -33,66 +38,80 @@ class KeopsNDFT:
 
     """
 
-    def __init__(self, samples, shape):
-        self.samples = proper_trajectory(samples, "unit")
+    def __init__(self, samples, shape, keops_backend="auto"):
+        self.samples = proper_trajectory(samples, "pi").astype(np.float32)
         self.shape = shape
-        self.dim = samples.shape[1]
+        self.dim = samples.shape[-1]
         self.dtype = samples.dtype
 
         if isinstance(samples, np.ndarray):
+            self._LazyTensor = NumpyLazyTensor
+            self._cLazyTensor = NumpyComplexLazyTensor
             self._Genred = NumpyGenred
-            self._LazyTensor = NumpyComplexLazyTensor
         elif PYTORCH_AVAILABLE and isinstance(samples, torch.Tensor):
+            self._LazyTensor = TorchLazyTensor
+            self._cLazyTensor = TorchComplexLazyTensor
             self._Genred = TorchGenred
-            self._LazyTensor = TorchComplexLazyTensor
         else:
             raise ValueError("Samples must be a numpy or torch array")
 
-        self._precomp_samples = self._LazyTensor(2 * np.pi * 1j * self.samples, axis=0)
-
         # location in the image domain.
-        self._image_loc = self._LazyTensor(
-            np.array(np.meshgrid(*[np.arange(s) for s in self.shape]))
+        self._locs = (
+            np.array(np.meshgrid(*[np.arange(s) for s in self.shape], indexing="ij"))
             .reshape(self.dim, -1)
-            .T
+            .T.astype(np.float32)
         )
 
+        # Fourier Transform image -> kspace
+        variables = ["x_j = Vj(1,{dim})", "nu_i = Vi(0,{dim})", "b_j = Vi(2,2)"]
+        aliases = [s.format(dim=self.dim) for s in variables]
         self._op = self._Genred(
-            "Sum_Reduction(x * Exp( - nu | px ))",
-            variable=["x = Vi(1,1)", "nu = Vj(1,2)" "px = Vi(1,2)"],
-            axis=0,
+            "ComplexMult(ComplexExp1j( - nu_i | x_j),  b_j)",
+            aliases,
+            reduction_op="Sum",
+            axis=1,
+            backend=keops_backend,
         )
 
+        # Adjoint Fourier Transform kspace -> image
+        variables = ["x_i = Vi(0,{dim})", "nu_j = Vj(1,{dim})", "c_j = Vj(2,2)"]
+        aliases = [s.format(dim=self.dim) for s in variables]
         self._adj_op = self._Genred(
-            "Sum_Reduction(x * Exp( + nu | px ))",
-            variable=["x = Vi(1,1)", "nu = Vj(1,2)" "px = Vi(1,2)"],
+            "ComplexMult(ComplexExp1j(nu_j | x_i), c_j)",
+            aliases,
+            reduction_op="Sum",
             axis=1,
+            backend=keops_backend,
         )
 
     def op(self, coeffs, image):
         """Apply the Fourier transform."""
-        image_ = self._LazyTensor(image)
+        image_ = image.astype(np.complex64).reshape(-1, 1)
         # Compute the Fourier transform
-        coeffs = self._op(image_.flatten(), self._precomp_samples, self._image_loc)
+        coeffs = self._op(self.samples, self._locs, image_).view(np.complex64)
         return coeffs
 
     def adj_op(self, coeffs, image):
         """Apply the adjoint Fourier transform."""
-        coeffs_ = self._LazyTensor(coeffs)
+        coeffs_ = coeffs.astype(np.complex64).reshape(-1, 1)
+
         # Compute the adjoint Fourier transform
-        image = self._adj_op(coeffs_, self._precomp_samples, self._image_loc)
+        image = self._adj_op(self._locs, self.samples, coeffs_).view(np.complex64)
+
         return image.reshape(self.shape)
 
 
 class MRIKeops(FourierOperatorCPU):
+    """MRI Fourier operator using Keops."""
+
     backend = "keops"
 
-    def __init__(self, samples, shape, n_coils, smaps=None):
+    def __init__(self, samples, shape, n_coils, smaps=None, **kwargs):
         super().__init__(
             samples,
             shape,
             density=False,
             n_coils=n_coils,
             smaps=smaps,
-            raw_op=KeopsNDFT(samples, shape),
+            raw_op=KeopsNDFT(samples, shape, **kwargs),
         )
