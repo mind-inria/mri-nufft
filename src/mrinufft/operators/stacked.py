d7c00a5ee3119fa472e33c9a183d4627538a59dc
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-09-14 11:57:54 +0200

    fix(stacked): make tests pass.

--- src/mrinufft/operators/stacked.py
+++ src/mrinufft/operators/stacked.py
@@ -61,7 +61,7 @@ class MRIStackedNUFFT(FourierOperatorBase):
         self.operator = get_operator(backend)(
             samples,
             shape[:-1],
-            n_coils=self.n_coils * self.n_batchs,
+            n_coils=self.n_coils,
             smaps=None,
             **kwargs,
         )
@@ -78,15 +78,16 @@ class MRIStackedNUFFT(FourierOperatorBase):
 
     def _fftz(self, data):
         """Apply FFT on z-axis."""
+        # sqrt(2) required for normalization
         return np.fft.fftshift(
             np.fft.fft(np.fft.ifftshift(data, axes=-1), axis=-1, norm="ortho"), axes=-1
-        )
+        ) / np.sqrt(2)
 
     def _ifftz(self, data):
         """Apply IFFT on z-axis."""
         return np.fft.fftshift(
             np.fft.ifft(np.fft.ifftshift(data, axes=-1), axis=-1, norm="ortho"), axes=-1
-        )
+        ) / np.sqrt(2)
 
     def op(self, data, ksp=None):
         """Forward operator."""
@@ -94,48 +95,41 @@ class MRIStackedNUFFT(FourierOperatorBase):
             return self._op_sense(data, ksp)
         else:
             return self._op_calibless(data, ksp)
-        # Do SENSE Stuff if needed.
-        # Apply the FFT on z-axis
-        # Apply the NUFFT on the selected plans
-        #
 
     def _op_sense(self, data, ksp=None):
         """Apply SENSE operator."""
         ksp = ksp or np.zeros(
-            (self.n_coils, len(self.samples) * len(self.z_index)),
+            (self.n_batchs, self.n_coils, len(self.samples), len(self.z_index)),
             dtype=self.cpx_dtype,
         )
-        data_ = np.reshape(self.n_batchs, *self.shape)
+        data_ = data.reshape(self.n_batchs, *self.shape)
         # TODO Add  batch support
         for b in range(self.n_batchs):
             data_c = data_[b] * self.smaps
             ksp_z = self._fftz(data_c)
-            ksp_z = ksp_z.reshape(self.operator.n_coils, *self.shape)
+            ksp_z = ksp_z.reshape(self.n_coils, *self.shape)
             for i, zidx in enumerate(self.z_index):
-                self.operator.op(ksp_z[..., zidx], ksp[:, :, i])
+                self.operator.op(ksp_z[..., zidx], ksp[b, ..., i])
         ksp = ksp.reshape(self.n_batchs, self.n_coils, self.n_samples)
         return ksp
 
     def _op_calibless(self, data, ksp=None):
         if ksp is None:
             ksp = np.empty(
-                (self.n_coils, len(self.samples), len(self.z_index)),
+                (self.n_batchs, self.n_coils, len(self.samples), len(self.z_index)),
                 dtype=self.cpx_dtype,
             )
         data_ = data.reshape((self.n_batchs, self.n_coils, *self.shape))
         ksp_z = self._fftz(data_)
-        ksp_z = ksp_z.reshape((self.operator.n_coils, *self.shape))
-        for i, zidx in enumerate(self.z_index):
-            self.operator.op(ksp_z[..., zidx], ksp[:, :, i])
+        ksp_z = ksp_z.reshape((self.n_batchs, self.n_coils, *self.shape))
+        for b in range(self.n_batchs):
+            for i, zidx in enumerate(self.z_index):
+                self.operator.op(ksp_z[b, ..., zidx], ksp[b, ..., i])
         ksp = ksp.reshape(self.n_batchs, self.n_coils, self.n_samples)
         return ksp
 
     def adj_op(self, coeffs, img=None):
         """Adjoint operator."""
-        # DO NUFFT adjoint
-        # Apply the FFT on z-axis
-        # Do SENSE Stuff if needed.
-
         coeffs_ = np.reshape(
             coeffs, (self.n_batchs, self.n_coils, len(self.samples), len(self.z_index))
         )
@@ -146,23 +140,25 @@ class MRIStackedNUFFT(FourierOperatorBase):
 
     def _adj_op_sense(self, coeffs, img):
         imgz = np.zeros(
-            (self.n_batchs * self.n_coils, *self.shape), dtype=self.cpx_dtype
+            (self.n_batchs, self.n_coils, *self.shape), dtype=self.cpx_dtype
         )
-        for i, zidx in enumerate(self.z_index):
-            self.operator.adj_op(coeffs[..., i], imgz[..., zidx])
-        imgz = np.reshape(imgz, self.n_batchs, self.n_coils, *self.shape)
+        for b in range(self.n_batchs):
+            for i, zidx in enumerate(self.z_index):
+                self.operator.adj_op(coeffs[b, ..., i], imgz[b, ..., zidx])
+        imgz = imgz.reshape(self.n_batchs, self.n_coils, *self.shape)
         imgc = self._ifftz(imgz)
-        img = img or np.empty(self.n_batchs, *self.shape, dtype=self.cpx_dtype)
-        for b in self.n_batchs:
+        img = img or np.empty((self.n_batchs, *self.shape), dtype=self.cpx_dtype)
+        for b in range(self.n_batchs):
             img[b] = np.sum(imgc[b] * self.smaps.conj(), axis=0)
         return img
 
     def _adj_op_calibless(self, coeffs, img):
         imgz = np.zeros(
-            (self.n_batchs * self.n_coils, *self.shape), dtype=self.cpx_dtype
+            (self.n_batchs, self.n_coils, *self.shape), dtype=self.cpx_dtype
         )
-        for i, zidx in enumerate(self.z_index):
-            self.operator.adj_op(coeffs[..., i], imgz[..., zidx])
+        for b in range(self.n_batchs):
+            for i, zidx in enumerate(self.z_index):
+                self.operator.adj_op(coeffs[b, ..., i], imgz[b, ..., zidx])
         imgz = np.reshape(imgz, (self.n_batchs, self.n_coils, *self.shape))
         img = self._ifftz(imgz)
         return img
