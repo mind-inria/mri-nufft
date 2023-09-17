d8f796cd32f5a67d37f72c8978f20192538fbbde
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-09-17 23:57:14 +0200

    feat: allow get_operator to return initialized operator.

--- src/mrinufft/operators/interfaces/__init__.py
+++ src/mrinufft/operators/interfaces/__init__.py
@@ -20,17 +20,20 @@ def check_backend(backend_name: str):
         raise ValueError(f"unknown backend: '{backend_name}'") from e
 
 
-def get_operator(backend_name: str):
+def get_operator(backend_name: str, *args, **kwargs):
     """Return an MRI Fourier operator interface using the correct backend.
 
     Parameters
     ----------
     backend_name: str
-        Backend name,
+        Backend name
+    *args, **kwargs:
+        Arguments to pass to the operator constructor.
 
     Returns
     -------
-    FourierOperatorBase Interface
+    FourierOperator
+        class or instance of class if args or kwargs are given.
 
     Raises
     ------
@@ -42,6 +45,9 @@ def get_operator(backend_name: str):
         raise ValueError("backend is not available") from exc
     if not available:
         raise ValueError("backend is registered, but dependencies are not met.")
+
+    if args or kwargs:
+        operator = operator(*args, **kwargs)
     return operator
 
 
