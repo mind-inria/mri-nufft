0bd82f23250ec50cd1e9fcbe6ff181c2c1a068aa
Daval-G <guillaume.daval-frerot@epita.fr>
2023-09-26 12:32:52 +0200

    Add Seiffert spiral and shell trajectories (#36)
    Co-authored-by: Guillaume DAVAL-FREROT <gdavalfrerot@gmail.com>

--- src/mrinufft/trajectories/trajectory3D.py
+++ src/mrinufft/trajectories/trajectory3D.py
@@ -2,6 +2,8 @@
 import numpy as np
 import numpy.linalg as nl
 
+from scipy.special import ellipj, ellipk
+
 from .expansions import (
     stack_2D_to_3D_expansion,
     rotate_2D_to_3D_expansion,
@@ -17,9 +19,9 @@ from .trajectory2D import (
 from .utils import Ry, Rz, Rv, initialize_tilt, KMAX
 
 
-################################
-# 3D TRAJECTORY INITIALIZATION #
-################################
+############################
+# FREEFORM 3D TRAJECTORIES #
+############################
 
 
 def initialize_3D_from_2D_expansion(
@@ -95,7 +97,8 @@ def initialize_3D_cones(Nc, Ns, tilt="golden", in_out=False, nb_zigzags=5, width
     tilt : str, float, optional
         Tilt of the cones, by default "golden"
     in_out : bool, optional
-        Whether the cones are going in and out, by default False
+        Whether the curves are going in-and-out or start from the center,
+        by default False
     nb_zigzags : float, optional
         Number of zigzags of the cones, by default 5
     width : float, optional
@@ -137,13 +140,108 @@ def initialize_3D_cones(Nc, Ns, tilt="golden", in_out=False, nb_zigzags=5, width
     return trajectory.reshape((Nc, Ns, 3))
 
 
+def initialize_3D_seiffert_spiral(
+    Nc, Ns, curve_index=0.2, nb_revolutions=1, tilt="golden", in_out=False
+):
+    """Initialize 3D trajectories with modulated Seiffert spirals.
+
+    Initially introduced in [SMR18]_, but also proposed later as "Yarnball"
+    in [SB21]_ as a nod to [IN95]_. The implementation is based on work
+    from [Er00]_ and [Br09]_, using Jacobi elliptic functions rather than
+    auxiliary theta functions.
+
+    Parameters
+    ----------
+    Nc : int
+        Number of shots
+    Ns : int
+        Number of samples per shot
+    curve_index : float
+        Index controlling curve from 0 (flat) to 1 (curvy), by default 0.3
+    nb_revolutions : float
+        Number of revolutions, i.e. times the polar angle of the curves
+        passes through 0, by default 1
+    tilt : str, float, optional
+        Angle between shots around z-axis over precession, by default "golden"
+    in_out : bool
+        Whether the curves are going in-and-out or start from the center,
+        by default False
+
+    Returns
+    -------
+    array_like
+        3D Seiffert spiral trajectory
+
+    References
+    ----------
+    .. [IN95] Irarrazabal, Pablo, and Dwight G. Nishimura.
+       "Fast three dimensional magnetic resonance imaging."
+       Magnetic Resonance in Medicine 33, no. 5 (1995): 656-662.
+    .. [Er00] Erdös, Paul.
+       "Spiraling the earth with C. G. J. jacobi."
+       American Journal of Physics 68, no. 10 (2000): 888-895.
+    .. [Br09] Brizard, Alain J.
+       "A primer on elliptic functions with applications in classical mechanics."
+       European journal of physics 30, no. 4 (2009): 729.
+    .. [SMR18] Speidel, Tobias, Patrick Metze, and Volker Rasche.
+       "Efficient 3D Low-Discrepancy k-Space Sampling
+       Using Highly Adaptable Seiffert Spirals."
+       IEEE Transactions on Medical Imaging 38, no. 8 (2018): 1833-1840.
+    .. [SB21] Stobbe, Robert W., and Christian Beaulieu.
+       "Three‐dimensional Yarnball k‐space acquisition for accelerated MRI."
+       Magnetic Resonance in Medicine 85, no. 4 (2021): 1840-1854.
+
+    """
+    # Normalize ellipses integrations by the requested period
+    trajectory = np.zeros((Nc, Ns // (1 + in_out), 3))
+    period = 4 * ellipk(curve_index**2)
+    times = np.linspace(0, nb_revolutions * period, Ns // (1 + in_out), endpoint=False)
+
+    # Initialize first shot
+    jacobi = ellipj(times, curve_index**2)
+    trajectory[0, :, 0] = jacobi[0] * np.cos(curve_index * times)
+    trajectory[0, :, 1] = jacobi[0] * np.sin(curve_index * times)
+    trajectory[0, :, 2] = jacobi[1]
+
+    # Make it volumetric instead of just a sphere surface
+    magnitudes = np.sqrt(np.linspace(0, 1, Ns // (1 + in_out)))
+    trajectory = magnitudes.reshape((1, -1, 1)) * trajectory
+
+    # Determine mostly evenly distributed points on sphere
+    points = np.zeros((Nc, 3))
+    phi = initialize_tilt(tilt) * np.arange(Nc)
+    points[:, 0] = np.linspace(-1, 1, Nc)
+    radius = np.sqrt(1 - points[:, 0] ** 2)
+    points[:, 1] = np.cos(phi) * radius
+    points[:, 2] = np.sin(phi) * radius
+
+    # Rotate initial shot Nc times
+    for i in np.arange(1, Nc)[::-1]:
+        v1 = np.array((1, 0, 0))
+        v2 = points[i]
+        rotation = Rv(v1, v2, normalize=False)
+        trajectory[i] = (rotation @ trajectory[0].T).T
+
+    # Handle in_out case
+    if in_out:
+        first_half_traj = np.copy(trajectory)
+        first_half_traj = -first_half_traj[:, ::-1]
+        trajectory = np.concatenate([first_half_traj, trajectory], axis=1)
+    return KMAX * trajectory
+
+
+###############################
+# SHELL-BASED 3D TRAJECTORIES #
+###############################
+
+
 def initialize_3D_helical_shells(
     Nc, Ns, nb_shells, spiral_reduction=1, shell_tilt="intergaps", shot_tilt="uniform"
 ):
     """Initialize 3D trajectories with helical shells.
 
     The implementation follows the proposition from [YRB06]_
-    but the idea can be traced back much further [PN95]_.
+    but the idea can be traced back much further [IN95]_.
 
     Parameters
     ----------
@@ -170,7 +268,7 @@ def initialize_3D_helical_shells(
     .. [YRB06] Shu, Yunhong, Stephen J. Riederer, and Matt A. Bernstein.
        "Three‐dimensional MRI with an undersampled spherical shells trajectory."
        Magnetic Resonance in Medicine 56, no. 3 (2006): 553-562.
-    .. [PN95] Irarrazabal, Pablo, and Dwight G. Nishimura.
+    .. [IN95] Irarrazabal, Pablo, and Dwight G. Nishimura.
        "Fast three dimensional magnetic resonance imaging."
        Magnetic Resonance in Medicine 33, no. 5 (1995): 656-662
 
@@ -331,6 +429,104 @@ def initialize_3D_annular_shells(
         count += Ms
 
     return KMAX * trajectory
+
+
+def initialize_3D_seiffert_shells(
+    Nc,
+    Ns,
+    nb_shells,
+    curve_index=0.5,
+    nb_revolutions=1,
+    shell_tilt="uniform",
+    shot_tilt="uniform",
+):
+    """Initialize 3D trajectories with Seiffert shells.
+
+    The implementation is based on work from [Er00]_ and [Br09]_,
+    using Jacobi elliptic functions to define Seiffert spirals
+    over shell/sphere surfaces.
+
+    Parameters
+    ----------
+    Nc : int
+        Number of shots
+    Ns : int
+        Number of samples per shot
+    nb_shells : int
+        Number of concentric shells/spheres
+    curve_index : float
+        Index controlling curve from 0 (flat) to 1 (curvy), by default 0.5
+    nb_revolutions : float
+        Number of revolutions, i.e. times the curve passes through the upper-half
+        of the z-axis, by default 1
+    shell_tilt : str, float, optional
+        Angle between consecutive shells along z-axis, by default "intergaps"
+    shot_tilt : str, float, optional
+        Angle between shots over a shell surface along z-axis, by default "uniform"
+
+    Returns
+    -------
+    array_like
+        3D Seiffert shell trajectory
+
+    References
+    ----------
+    .. [IN95] Irarrazabal, Pablo, and Dwight G. Nishimura.
+       "Fast three dimensional magnetic resonance imaging."
+       Magnetic Resonance in Medicine 33, no. 5 (1995): 656-662.
+    .. [Er00] Erdös, Paul.
+       "Spiraling the earth with C. G. J. jacobi."
+       American Journal of Physics 68, no. 10 (2000): 888-895.
+    .. [Br09] Brizard, Alain J.
+       "A primer on elliptic functions with applications in classical mechanics."
+       European journal of physics 30, no. 4 (2009): 729.
+
+    """
+    # Check arguments validity
+    if Nc < nb_shells:
+        raise ValueError("Argument nb_shells should not be higher than Nc.")
+    trajectory = np.zeros((Nc, Ns, 3))
+
+    # Attribute shots to shells following a prescribed density
+    Nc_per_shell = np.ones(nb_shells).astype(int)
+    density = np.arange(1, nb_shells + 1) ** 2  # simplified version
+    for _ in range(Nc - nb_shells):
+        idx = np.argmax(density / Nc_per_shell)
+        Nc_per_shell[idx] += 1
+
+    # Normalize ellipses integrations by the requested period
+    period = 4 * ellipk(curve_index**2)
+    times = np.linspace(0, nb_revolutions * period, Ns, endpoint=False)
+
+    # Create shells one by one
+    count = 0
+    radii = (0.5 + np.arange(nb_shells)) / nb_shells
+    for i in range(nb_shells):
+        # Prepare shell parameters
+        Ms = Nc_per_shell[i]
+        k0 = radii[i]
+
+        # Initialize first shot
+        jacobi = ellipj(times, curve_index**2)
+        trajectory[count, :, 0] = k0 * jacobi[0] * np.cos(curve_index * times)
+        trajectory[count, :, 1] = k0 * jacobi[0] * np.sin(curve_index * times)
+        trajectory[count, :, 2] = k0 * jacobi[1]
+
+        # Rotate first shot Ms times to create the shell
+        rotation = Rz(initialize_tilt(shot_tilt, Ms))
+        for j in range(1, Ms):
+            trajectory[count + j] = trajectory[count + j - 1] @ rotation
+
+        # Apply shell tilt
+        rotation = Rz(i * initialize_tilt(shell_tilt, nb_shells))
+        trajectory[count : count + Ms] = trajectory[count : count + Ms] @ rotation
+        count += Ms
+    return KMAX * trajectory
+
+
+#########
+# UTILS #
+#########
 
 
 def duplicate_per_axes(trajectory, axes=(0, 1, 2)):
