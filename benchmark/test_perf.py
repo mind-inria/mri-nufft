a66d929a96cd9b4f3049fb0bd3006ca3360dc5ad
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-10-27 15:20:55 +0200

    parametrize by avaiable backends.

--- benchmark/test_perf.py
+++ benchmark/test_perf.py
@@ -1,34 +1,41 @@
 """Test the performance of the operators."""
 from pytest_cases import parametrize_with_cases, parametrize, fixture
 import numpy as np
-from mrinufft import get_operator
+from mrinufft import get_operator, list_backends
 from mrinufft.trajectories import initialize_2D_radial
 from mrinufft.operators.stacked import stacked2traj3d
 
 
-def traj_radial2D(Nc=10, Ns=500):
+def traj_radial2D(Nc=16, Ns=512):
     """Create a 2D radial trajectory."""
     trajectory = initialize_2D_radial(Nc, Ns)
     return trajectory
+
+
+N_COILS_BIG = 32
+N_COILS_SMALL = 4
+
+BACKENDS = [s for s in list_backends(True) if "stacked" not in s]
+SHAPE = ()
 
 
 @fixture(scope="module")
 @parametrize_with_cases("kspace_locs", cases=".", prefix="traj_")
 @parametrize(
     "backend, backend_kwargs",
-    [
-        ("finufft", None),
-        # ("sigpy", None),
-        # ("cufinufft", {}),
-        # ("gpunufft", {}),
-        ("stacked", "{'backend':'finufft'}"),
-        # ("stacked-cufinufft", {}),
-    ],
+    [(b, None) for b in BACKENDS]
+    + [("stacked", "{'backend':'finufft'}"), "stacke-cufinufft"],
 )
 @parametrize("z_index", ["full", "random_mask"])
 @parametrize(
     "n_coils, sense",
-    [(1, False), (4, False), (4, True)],
+    [
+        (1, False),
+        (N_COILS_SMALL, False),
+        (N_COILS_SMALL, True),
+        (N_COILS_BIG, False),
+        (N_COILS_BIG, True),
+    ],
 )
 def operator(
     request,
@@ -40,9 +47,7 @@ def operator(
     sense,
 ):
     """Initialize the stacked and non-stacked operators."""
-    shape = (64, 64)
-    shape3d = (*shape, shape[-1])  # add a 3rd dimension
-
+    shape3d = SHAPE
     if backend_kwargs:
         backend_kwargs = eval(backend_kwargs)
     else:
@@ -50,7 +55,7 @@ def operator(
     if z_index == "full":
         z_index = np.arange(shape3d[-1])
     elif z_index == "random_mask":
-        z_index = np.random.choice(shape3d[-1], size=shape3d[-1] // 2, replace=False)
+        z_index = np.random.choice(shape3d[-1], size=shape3d[-1] // 4, replace=False)
     # smaps support
     if sense:
         smaps = 1j * np.random.rand(n_coils, *shape3d)
@@ -68,7 +73,7 @@ def operator(
             **backend_kwargs,
         )
 
-    kspace_locs3d = stacked2traj3d(kspace_locs, z_index, shape[-1])
+    kspace_locs3d = stacked2traj3d(kspace_locs, z_index, shape3d[-1])
     return get_operator(backend)(
         kspace_locs3d,
         shape=shape3d,
