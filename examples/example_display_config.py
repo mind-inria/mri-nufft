5e9748eb49a659a6d141cfbbe5a2776b98d32a41
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-11-20 15:09:03 +0100

    feat: start example file.

---
+++ examples/example_display_config.py
@@ -0,0 +1,29 @@
+"""
+================================
+Trajectory display configuration
+================================
+
+The look of the display trajectories can be tweaked by using :py:class:`displayConfig`
+
+You can tune these parameters to your own taste and needs.
+"""
+
+# %%
+
+from mrinufft import displayConfig, display_2D_trajectory, display_3D_trajectory
+from mrinufft.trajectories import initialize_2D_radial, initialize_3D_cones
+
+
+# %%
+#
+# Trajectory displays
+# ====================
+
+# %%
+# Labels, titles and legends
+# ==========================
+
+
+# %%
+# Gradients profiles
+# ==================
