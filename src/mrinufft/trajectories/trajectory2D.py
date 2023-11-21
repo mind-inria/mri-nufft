ba0154746d6d045501a5cbc4d2e699bf6bc39f01
Guillaume DAVAL-FREROT <gdavalfrerot@gmail.com>
2023-11-21 23:20:46 +0100

    Add documentation

--- src/mrinufft/trajectories/trajectory2D.py
+++ src/mrinufft/trajectories/trajectory2D.py
@@ -166,20 +166,43 @@ def initialize_2D_sinusoide(
 
 
 def initialize_2D_propeller(Nc, Ns, nb_strips, tilt="uniform"):
+    """Initialize a 2D PROPELLER trajectory, as proposed in []_.
+
+    The PROPELLER trajectory is generally used along a specific
+    reconstruction pipeline described in []_ to correct for
+    motion artifacts.
+
+    The acronym PROPELLER stands for Periodically Rotated
+    Overlapping ParallEL Lines with Enhanced Reconstruction,
+    and the method is also commonly known under other aliases
+    depending on the vendor, with some variations: BLADE,
+    MulitVane, RADAR, JET.
+
+    Parameters
+    ----------
+    Nc : int
+        Number of shots
+    Ns : int
+        Number of samples per shot
+    nb_strips : int
+        Number of rotated strips, must divide ``Nc``.
+    tilt : str, float, optional
+        Tilt of the strips, by default "uniform"
+    """
     # Check for value errors
     if Nc % nb_strips != 0:
         raise ValueError("Nc should be divisible by nb_strips.")
 
     # Initialize single shot
-    Nc_per_band = Nc // nb_strips
+    Nc_per_strip = Nc // nb_strips
     trajectory = np.linspace(-1, 1, Ns).reshape((1, Ns, 1))
 
     # Convert single shot to single strip
-    trajectory = np.tile(trajectory, reps=(Nc_per_band, 1, 2))
-    y_axes = np.pi / 2 / nb_strips * np.linspace(-1, 1, Nc_per_band)
+    trajectory = np.tile(trajectory, reps=(Nc_per_strip, 1, 2))
+    y_axes = np.pi / 2 / nb_strips * np.linspace(-1, 1, Nc_per_strip)
     trajectory[:, :, 1] = y_axes[:, None]
 
-    # Rotate single band into multiple strips
+    # Rotate single strip into multiple strips
     trajectory = rotate(trajectory, nb_rotations=nb_strips, z_tilt=np.pi / nb_strips)
     trajectory = trajectory[..., :2]  # Remove dim added by rotate
 
