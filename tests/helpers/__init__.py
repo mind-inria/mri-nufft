03e7d18e6e34ca64208d15470a00c2c594e6e594
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-11-23 21:37:22 +0100

    feat(test): refactor test to follow changes of api.

--- tests/helpers/__init__.py
+++ tests/helpers/__init__.py
@@ -1,4 +1,11 @@
-#!/usr/bin/env python3
+"""Helper functions for testing the operators."""
 
-from .asserts import assert_almost_allclose
+from .asserts import assert_almost_allclose, assert_correlate
 from .factories import kspace_from_op, image_from_op
+
+__all__ = [
+    "assert_almost_allclose",
+    "assert_correlate",
+    "kspace_from_op",
+    "image_from_op",
+]
