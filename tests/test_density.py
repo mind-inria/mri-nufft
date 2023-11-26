738509e7ad44e7d190f97fd8b2ade11b30ebc4a8
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-11-26 17:58:52 +0100

    feat: just test for reproductibility.

--- tests/test_density.py
+++ tests/test_density.py
@@ -14,7 +14,7 @@ from case_trajectories import CasesTrajectories
 def test_density_pipe(kspace_traj, shape, backend):
     """Test the density compensation estimations."""
     density = pipe(kspace_traj, shape, backend=backend, num_iter=20, tol=2e-7).get()
-    density_ref = pipe(kspace_traj, shape, backend=backend, num_iter=25, tol=2e-7).get()
+    density_ref = pipe(kspace_traj, shape, backend=backend, num_iter=20, tol=2e-7).get()
 
-    # TODO: get tighter bounds.
+    # TODO: get tighter bounds. check for convergence etc
     npt.assert_allclose(density, density_ref, atol=1, rtol=1)
