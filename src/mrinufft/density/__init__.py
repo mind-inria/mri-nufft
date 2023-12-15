5ad09f4bc7b3fedaf254de194fbfaead4fb5ced9
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-12-15 18:10:28 +0100

    feat: dispatch density estimation method.

--- src/mrinufft/density/__init__.py
+++ src/mrinufft/density/__init__.py
@@ -1,6 +1,13 @@
 """Density compensation methods."""
 from .geometry_based import voronoi, voronoi_unique, cell_count
 from .nufft_based import pipe
+from .utils import get_density
 
 
-__all__ = ["voronoi", "voronoi_unique", "pipe", "cell_count"]
+__all__ = [
+    "voronoi",
+    "voronoi_unique",
+    "pipe",
+    "cell_count",
+    "get_density",
+]
