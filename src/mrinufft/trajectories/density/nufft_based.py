9d4e060d15a679d07d75f167e9e6cf72b6f3c113
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-12-15 10:03:58 +0100

    feat: enforce common api for density compensation functions.

--- src/mrinufft/trajectories/density/nufft_based.py
+++ src/mrinufft/trajectories/density/nufft_based.py
@@ -1,21 +1,23 @@
 """Density compensation weights using the NUFFT-based methods."""
 
 from mrinufft import get_operator
+from .utils import flat_traj
 
 
-def pipe(kspace_traj, grid_size, backend="cufinufft", **kwargs):
+@flat_traj
+def pipe(traj, shape, backend="cufinufft", **kwargs):
     """Compute the density compensation weights using the pipe method.
 
     Parameters
     ----------
-    kspace_traj: array_like
+    traj: array_like
         array of shape (M, 2) or (M, 3) containing the coordinates of the points.
-    grid_size: array_like
+    shape: array_like
         array of shape (2,) or (3,) containing the size of the grid.
     backend: str
         backend to use for the computation. Either "cufinufft" or "tensorflow".
     """
     nufft_class = get_operator(backend)
     if hasattr(nufft_class, "pipe"):
-        return nufft_class.pipe(kspace_traj, grid_size, **kwargs)
+        return nufft_class.pipe(traj, shape, **kwargs)
     raise ValueError("backend does not have pipe iterations method.")
