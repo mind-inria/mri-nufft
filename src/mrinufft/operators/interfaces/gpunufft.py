45bc400b2cfec8abee4c550780f7a6e5f1fa433c
chaithyagr <chaithyagr@gitlab.com>
2024-05-23 17:48:10 +0200

    Move to tuple

--- src/mrinufft/operators/interfaces/gpunufft.py
+++ src/mrinufft/operators/interfaces/gpunufft.py
@@ -511,7 +511,7 @@ class MRIGpuNUFFT(FourierOperatorBase):
         )
         if normalize:
             spike = np.zeros(volume_shape)
-            mid_loc = (v//2 for v in volume_shape)
+            mid_loc = tuple(v//2 for v in volume_shape)
             spike[mid_loc] = 1
             psf = grid_op.adj_op(grid_op.op(spike))
             density_comp /= np.linalg.norm(psf)
