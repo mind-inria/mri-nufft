0138abdce49ea84ca4fabf4647e401b02677ff0e
Caini PAN <cp279020@is243416.intra.cea.fr>
2024-05-24 15:30:04 +0200

    update for the

--- src/mrinufft/operators/autodiff.py
+++ src/mrinufft/operators/autodiff.py
@@ -3,21 +3,54 @@
 import torch
 
 
+# class _NUFFT_OP(torch.autograd.Function):
+#     """Autograd support for op nufft function."""
+
+#     @staticmethod
+#     def forward(ctx, x, nufft_op):
+#         """Forward image -> k-space."""
+#         ctx.save_for_backward(x)
+#         ctx.nufft_op = nufft_op
+#         return nufft_op.op(x)
+
+#     @staticmethod
+#     def backward(ctx, dy):
+#         """Backward image -> k-space."""
+#         (x,) = ctx.saved_tensors
+
+#         return ctx.nufft_op.adj_op(dy),  None 
+        
 class _NUFFT_OP(torch.autograd.Function):
     """Autograd support for op nufft function."""
 
     @staticmethod
-    def forward(ctx, x, nufft_op):
+    def forward(ctx, x, nufft_op, traj):
         """Forward image -> k-space."""
-        ctx.save_for_backward(x)
+        ctx.save_for_backward(x, traj)
         ctx.nufft_op = nufft_op
         return nufft_op.op(x)
+    
 
     @staticmethod
     def backward(ctx, dy):
         """Backward image -> k-space."""
-        (x,) = ctx.saved_tensors
-        return ctx.nufft_op.adj_op(dy), None
+        print(dy.shape)
+        (x,traj) = ctx.saved_tensors 
+
+        im_size = x.size()[1:] #[16, 16]
+        r  = [torch.linspace(-size / 2, size / 2 - 1, size) for size in im_size] #len(r) = 2 / [16] for each
+        grid_r = torch.meshgrid(*r, indexing='ij')
+        grid_r = torch.stack(grid_r, dim=0).type_as(x)[None, ...]# add batch size [1, 2, 16, 16]
+
+        grid_x = x * grid_r
+        nufft_dx_dom = torch.cat([ctx.nufft_op.op(grid_x[:, i:i+1, :, :]) for i in range(grid_x.size(1))], dim=1)
+        #nufft_dx_dom = ctx.nufft_op.op(x * grid_r) # not work beacuse op only accpect [1, 1, *im_size]
+        
+        grad_traj = torch.transpose((-1j * torch.conj(dy) * nufft_dx_dom).squeeze(), 0, 1).type_as(traj) #dy should be [1, 1, 256] nufft_dx_dom should be [1, 2, 256] the first dim is batch size which should be reserved for the nufft
+    
+
+
+        return ctx.nufft_op.adj_op(dy), None, grad_traj     
 
 
 class _NUFFT_ADJOP(torch.autograd.Function):
@@ -52,9 +85,9 @@ class MRINufftAutoGrad(torch.nn.Module):
             raise ValueError("Squeezing dimensions is not " "supported for autodiff.")
         self.nufft_op = nufft_op
 
-    def op(self, x):
+    def op(self, x, traj):
         r"""Compute the forward image -> k-space."""
-        return _NUFFT_OP.apply(x, self.nufft_op)
+        return _NUFFT_OP.apply(x, self.nufft_op, traj)
 
     def adj_op(self, kspace):
         r"""Compute the adjoint k-space -> image."""
--- tests/test_autodiff.py
+++ tests/test_autodiff.py
@@ -6,7 +6,7 @@ import pytest
 from pytest_cases import parametrize_with_cases, parametrize, fixture
 from case_trajectories import CasesTrajectories
 from mrinufft.operators import get_operator
-
+import warnings
 
 from helpers import (
     kspace_from_op,
@@ -46,10 +46,61 @@ def operator(kspace_loc, shape, backend, autograd):
     return nufft
 
 
+def proper_trajectory_torch(trajectory, normalize="pi"):
+    
+    if not torch.is_tensor(trajectory):
+        raise ValueError("trajectory should be a torch.Tensor")
+
+    new_traj = trajectory.clone()
+    new_traj = new_traj.view(-1, trajectory.shape[-1])
+
+    if normalize == "pi" and torch.max(torch.abs(new_traj)) - 1e-4 < 0.5:
+        warnings.warn(
+            "Samples will be rescaled to [-pi, pi), assuming they were in [-0.5, 0.5)"
+        )
+        new_traj *= 2 * torch.pi
+    elif normalize == "unit" and torch.max(torch.abs(new_traj)) - 1e-4 > 0.5:
+        warnings.warn(
+            "Samples will be rescaled to [-0.5, 0.5), assuming they were in [-pi, pi)"
+        )
+        new_traj /= 2 * torch.pi
+
+    if normalize == "unit" and torch.max(new_traj) >= 0.5:
+        new_traj = (new_traj + 0.5) % 1 - 0.5
+
+    return new_traj
+
+# We need the calculation of the NDFT matrix to be done in the GPU/CPU, so I rewrite this function to calculate it on GPU/CPU (it's for grad_traj backpropagation, the
+# whole things about trajectory should be done with required_grad=True which needs everthing to be put on the same device)
+def get_fourier_matrix_torch(ktraj, shape, dtype=torch.complex64, normalize=False):
+    """Get the NDFT Fourier Matrix."""
+    device = ktraj.device
+    ktraj = proper_trajectory_torch(ktraj, normalize="unit")
+    n = np.prod(shape)
+    ndim = len(shape)
+    
+    r = [torch.linspace(-s / 2, s / 2 - 1, s, device=device) for s in shape]
+    
+    grid_r = torch.meshgrid(r, indexing="ij")
+    grid_r = torch.reshape(torch.stack(grid_r), (ndim, n)).to(device)
+    
+    traj_grid = torch.matmul(ktraj, grid_r)
+    matrix = torch.exp(-2j * np.pi * traj_grid).to(dtype).to(device).clone()
+    
+    if normalize:
+        matrix /= torch.sqrt(torch.tensor(np.prod(shape), device=device)) * torch.pow(torch.sqrt(torch.tensor(2, device=device)), ndim)
+    
+    return matrix
+
+def ndft_matrix_ktraj(operator, k_traj):
+    """Get the NDFT matrix from the operator."""
+    return get_fourier_matrix_torch(k_traj, operator.shape, normalize=True) # operator.samples is trajectory
+
+
 @fixture(scope="module")
 def ndft_matrix(operator):
     """Get the NDFT matrix from the operator."""
-    return get_fourier_matrix(operator.samples, operator.shape, normalize=True)
+    return get_fourier_matrix(operator.samples, operator.shape, normalize=True) 
 
 
 @pytest.mark.parametrize("interface", ["torch-gpu", "torch-cpu"])
@@ -81,24 +132,38 @@ def test_adjoint_and_grad(operator, ndft_matrix, interface):
 
 @pytest.mark.parametrize("interface", ["torch-gpu", "torch-cpu"])
 @pytest.mark.skipif(not TORCH_AVAILABLE, reason="Pytorch is not installed")
-def test_forward_and_grad(operator, ndft_matrix, interface):
+def test_forward_and_grad(operator, interface):
     """Test the adjoint and gradient of the operator."""
     if operator.backend == "finufft" and "gpu" in interface:
         pytest.skip("GPU not supported for finufft backend")
 
-    ndft_matrix_torch = to_interface(ndft_matrix, interface=interface)
-    ksp_data_ref = to_interface(kspace_from_op(operator), interface=interface)
-    img_data = to_interface(image_from_op(operator), interface=interface)
+
+    ktraj = to_interface(np.copy(operator.samples), interface = interface)
+    
+    ksp_data_ref = to_interface(kspace_from_op(operator), interface=interface) #[1, 256]
+    img_data = to_interface(image_from_op(operator), interface=interface) #[1, 16, 16] 1 is num of coil
+
     img_data.requires_grad = True
+    ktraj.requires_grad = True
 
     with torch.autograd.set_detect_anomaly(True):
-        ksp_data = operator.op(img_data).reshape(ksp_data_ref.shape)
-        ksp_data_ndft = (ndft_matrix_torch @ img_data.flatten()).reshape(ksp_data.shape)
+        ksp_data = operator.op(img_data, ktraj).reshape(ksp_data_ref.shape) #[1, 1, 256] => [1, 256]
+        ndft_matrix_torch = ndft_matrix_ktraj(operator,ktraj)
+        ksp_data_ndft = (ndft_matrix_torch @ img_data.flatten()).reshape(ksp_data.shape) 
         loss_nufft = torch.mean(torch.abs(ksp_data - ksp_data_ref) ** 2)
         loss_ndft = torch.mean(torch.abs(ksp_data_ndft - ksp_data_ref) ** 2)
 
+
+    # Check if nufft and ndft w.r.t trajectory are close in the backprop
+    gradient_ndft_ktraj = torch.autograd.grad(loss_ndft, ktraj, retain_graph=True)[0] 
+    gradient_nufft_ktraj= torch.autograd.grad(loss_nufft, ktraj, retain_graph=True)[  #d dy = dL / ksp_data, dy should be [1, 256]
+        0
+    ]
+    assert torch.allclose(gradient_ndft_ktraj, gradient_nufft_ktraj, atol=6e-3)
+
+
     # Check if nufft and ndft are close in the backprop
-    gradient_ndft_kdata = torch.autograd.grad(loss_ndft, img_data, retain_graph=True)[0]
+    gradient_ndft_kdata = torch.autograd.grad(loss_ndft, img_data, retain_graph=True)[0] 
     gradient_nufft_kdata = torch.autograd.grad(loss_nufft, img_data, retain_graph=True)[
         0
     ]
