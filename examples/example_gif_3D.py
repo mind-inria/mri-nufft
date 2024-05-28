99c0711c6010431f7e59f8fa92868b253e9e64fb
Guillaume Daval-Frérot <guillaume.daval-frerot@epita.fr>
2024-05-28 20:22:14 +0200

    Spiral patch (#105)
    * Change spiral parameterization, add method to patch abnormal gradients
    
    * Add 2D Fibonacci spiral, add examples for all spirals, improve gradient anomaly fix
    
    * Apply ruff/black formatting
    
    * Fix null power case
    
    * Update and clean gif examples
    
    * Make finding closest Fibonacci constant time search, update documentation
    
    * Apply black & ruff formatting
    
    ---------
    
    Co-authored-by: Guillaume DAVAL-FREROT <gdavalfrerot@gmail.com>

--- examples/example_gif_3D.py
+++ examples/example_gif_3D.py
@@ -7,97 +7,45 @@ A collection of 3D trajectories are generated and saved as a gif.
 
 """
 
-# %%
+import joblib
 import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image, ImageSequence
+
 import mrinufft.trajectories.display as mtd
 import mrinufft.trajectories.trajectory3D as mtt
-import numpy as np
-import joblib
-from PIL import Image, ImageSequence
 from mrinufft.trajectories.display import displayConfig
-from tqdm.notebook import tqdm
 
-# %% [markdown]
-# # Options
+# Options
 
-# %%
 Nc = 8 * 8
 Ns = 200
-
 nb_repetitions = 8
 
-# %%
 one_shot = 0
 figsize = 5
 
-nb_fps = 12
-name_slow = 1
+nb_frames = 3
+duration = 150  # seconds
 
-# %% [markdown]
-# # Generation
 
-# %%
+# Generation
+
 # Initialize trajectory function
 functions = [
     # 3D Cones
-    ("3D Cones", lambda x: mtt.initialize_3D_cones(Nc, Ns, nb_zigzags=0)[::-1]),
+    ("3D Cones", lambda x: mtt.initialize_3D_cones(Nc, Ns, width=x)[::-1]),
+    ("3D Cones", lambda x: mtt.initialize_3D_cones(Nc, Ns, width=x)[::-1]),
     ("3D Cones", lambda x: mtt.initialize_3D_cones(Nc, Ns, nb_zigzags=x)[::-1]),
-    ("3D Cones", lambda x: mtt.initialize_3D_cones(Nc, Ns, width=x)[::-1]),
-    ("3D Cones", lambda x: mtt.initialize_3D_cones(Nc, Ns, width=x)[::-1]),
+    ("3D Cones", lambda x: mtt.initialize_3D_cones(Nc, Ns, nb_zigzags=x)[::-1]),
     ("3D Cones", lambda x: mtt.initialize_3D_cones(Nc, Ns)[::-1]),
     # FLORET
-    (
-        "FLORET",
-        lambda x: mtt.initialize_3D_floret(
-            Nc // nb_repetitions,
-            Ns,
-            nb_revolutions=1,
-            max_angle=np.pi / 2,
-            axes=[0],
-            cone_tilt=None,
-        ),
-    ),
-    (
-        "FLORET",
-        lambda x: mtt.initialize_3D_floret(
-            x * Nc // nb_repetitions,
-            Ns,
-            nb_revolutions=x,
-            max_angle=np.pi / 2,
-            axes=[0],
-            cone_tilt=None,
-        ),
-    ),
-    (
-        "FLORET",
-        lambda x: mtt.initialize_3D_floret(
-            Nc, Ns, nb_revolutions=nb_repetitions, max_angle=x, axes=[0], cone_tilt=None
-        ),
-    ),
-    (
-        "FLORET",
-        lambda x: mtt.initialize_3D_floret(
-            Nc, Ns, nb_revolutions=nb_repetitions, max_angle=x, axes=[0], cone_tilt=None
-        ),
-    ),
-    # ("FLORET", lambda x: mtt.initialize_3D_floret(Nc, Ns, nb_cones=nb_repetitions, max_angle=np.pi / 2, nb_revolutions=x / 2, spiral=x, axes=[0], cone_tilt=None)),
-    # ("FLORET", lambda x: mtt.initialize_3D_floret(Nc, Ns, nb_cones=nb_repetitions, max_angle=np.pi / 2, nb_revolutions=x / 2, spiral=x, axes=[0], cone_tilt=None)),
-    (
-        "FLORET",
-        lambda x: mtt.initialize_3D_floret(
-            Nc,
-            Ns,
-            nb_revolutions=nb_repetitions,
-            max_angle=np.pi / 2,
-            axes=[0],
-            cone_tilt=None,
-        ),
-    ),
+    ("FLORET", lambda x: mtt.initialize_3D_floret(Nc, Ns, nb_revolutions=x)),
+    ("FLORET", lambda x: mtt.initialize_3D_floret(Nc, Ns, nb_revolutions=x)),
+    ("FLORET", lambda x: mtt.initialize_3D_floret(Nc, Ns, max_angle=x)),
+    ("FLORET", lambda x: mtt.initialize_3D_floret(Nc, Ns, max_angle=x)),
+    ("FLORET", lambda x: mtt.initialize_3D_floret(Nc, Ns)),
     # Seiffert spirals
-    (
-        "Seiffert spiral / Yarnball",
-        lambda x: mtt.initialize_3D_seiffert_spiral(Nc, Ns, curve_index=0),
-    ),
     (
         "Seiffert spiral / Yarnball",
         lambda x: mtt.initialize_3D_seiffert_spiral(Nc, Ns, curve_index=x),
@@ -105,19 +53,19 @@ functions = [
     (
         "Seiffert spiral / Yarnball",
         lambda x: mtt.initialize_3D_seiffert_spiral(
-            Nc, Ns, curve_index=0.9, nb_revolutions=x
+            Nc, Ns, curve_index=0.7, nb_revolutions=x
         ),
     ),
     (
         "Seiffert spiral / Yarnball",
         lambda x: mtt.initialize_3D_seiffert_spiral(
-            Nc, Ns, curve_index=0.9, nb_revolutions=x
+            Nc, Ns, curve_index=0.7, nb_revolutions=x
         ),
     ),
     (
         "Seiffert spiral / Yarnball",
         lambda x: mtt.initialize_3D_seiffert_spiral(
-            Nc, Ns, curve_index=0.9, nb_revolutions=1
+            Nc, Ns, curve_index=0.7, nb_revolutions=1
         ),
     ),
     # Helical shells
@@ -139,18 +87,6 @@ functions = [
             Nc, Ns, nb_shells=nb_repetitions, spiral_reduction=3
         )[::-1],
     ),
-    # Annular shells
-    # ("Annular shells", lambda x: mtt.initialize_3D_annular_shells(x * Nc // nb_repetitions, Ns, nb_shells=x, ring_tilt=0)[::-1]),
-    # ("Annular shells", lambda x: mtt.initialize_3D_annular_shells(Nc, Ns, nb_shells=nb_repetitions, ring_tilt=x)[::-1]),
-    # ("Annular shells", lambda x: mtt.initialize_3D_annular_shells(Nc, Ns, nb_shells=nb_repetitions, ring_tilt=x)[::-1]),
-    # ("Annular shells", lambda x: mtt.initialize_3D_annular_shells(Nc, Ns, nb_shells=nb_repetitions, ring_tilt=np.pi / 2)[::-1]),
-    # Seiffert shells
-    # ("Seiffert shells", lambda x: mtt.initialize_3D_seiffert_shells(x * Nc // nb_repetitions, Ns, nb_shells=x, curve_index=0)[::-1]),
-    # ("Seiffert shells", lambda x: mtt.initialize_3D_seiffert_shells(Nc, Ns, nb_shells=nb_repetitions, curve_index=x)[::-1]),
-    # ("Seiffert shells", lambda x: mtt.initialize_3D_seiffert_shells(Nc, Ns, nb_shells=nb_repetitions, curve_index=x)[::-1]),
-    # ("Seiffert shells", lambda x: mtt.initialize_3D_seiffert_shells(Nc, Ns, nb_shells=nb_repetitions, nb_revolutions=x)[::-1]),
-    # ("Seiffert shells", lambda x: mtt.initialize_3D_seiffert_shells(Nc, Ns, nb_shells=nb_repetitions, nb_revolutions=x)[::-1]),
-    # ("Seiffert shells", lambda x: mtt.initialize_3D_seiffert_shells(Nc, Ns, nb_shells=nb_repetitions, nb_revolutions=1)[::-1]),
     # Wave-CAIPI
     (
         "Wave-CAIPI",
@@ -164,57 +100,37 @@ functions = [
             2 * Nc, Ns, nb_revolutions=5 * x, width=x
         ),
     ),
-    # ("Wave-CAIPI", lambda x: mtt.initialize_3D_wave_caipi(2 * Nc, Ns, packing=["triangular", "square", "circular", "triangular"][x])),
     ("Wave-CAIPI", lambda x: mtt.initialize_3D_wave_caipi(2 * Nc, Ns)),
 ]
 
-# %%
 # Initialize trajectory arguments
 arguments = [
     # 3D Cones
-    [None] * (nb_fps // 4),  # None
-    np.linspace(0, np.sqrt(5), nb_fps) ** 2,  # nb_zigzags
-    np.linspace(1, 2, nb_fps // 2),  # width
-    np.linspace(2, 1, nb_fps // 2),  # width
-    [None] * (nb_fps // 4),  # None
+    np.linspace(0, 2, 4 * nb_frames),  # width
+    np.linspace(2, 1, 2 * nb_frames),  # width
+    np.linspace(np.sqrt(5), 1, 4 * nb_frames) ** 2,  # nb_zigzags
+    np.linspace(1, np.sqrt(5), 2 * nb_frames) ** 2,  # nb_zigzags
+    [None] * nb_frames,  # None
     # FLORET
-    [None] * (nb_fps // 4),  # None
-    np.around(np.linspace(1, nb_repetitions, nb_fps)).astype(int),  # nb_cones
-    np.linspace(np.pi / 2, np.pi / 4, nb_fps // 2),  # max_angle
-    np.linspace(np.pi / 4, np.pi / 2, nb_fps // 2),  # max_angle
-    # np.linspace(2, 1, nb_fps // 2), # spiral & nb_revolutions
-    # np.linspace(1, 2, nb_fps // 2), # spiral & nb_revolutions
-    [None] * (nb_fps // 4),  # None
+    np.linspace(1, 3, 4 * nb_frames),  # nb_revolutions
+    np.linspace(3, 1, 2 * nb_frames),  # nb_revolutions
+    np.linspace(np.pi / 2, np.pi / 4, 4 * nb_frames),  # max_angle
+    np.linspace(np.pi / 4, np.pi / 2, 2 * nb_frames),  # max_angle
+    [None] * nb_frames,  # None
     # Seiffert spiral
-    [None] * (nb_fps // 4),  # None
-    np.linspace(0, 0.9, nb_fps),  # curve_index
-    np.linspace(1, 3, nb_fps),  # nb_revolutions
-    np.linspace(3, 1, nb_fps // 2),  # nb_revolutions
-    [None] * (nb_fps // 4),  # None
+    np.linspace(0, 0.7, 4 * nb_frames),  # curve_index
+    np.linspace(1, 2, 4 * nb_frames),  # nb_revolutions
+    np.linspace(2, 1, 2 * nb_frames),  # nb_revolutions
+    [None] * nb_frames,  # None
     # Helical shells
-    np.around(np.linspace(1, nb_repetitions, nb_fps)).astype(int),  # nb_cones
-    np.linspace(1, 3, nb_fps),  # spiral_reduction
-    [None] * (nb_fps // 4),  # None
-    # Annular shells
-    # np.around(np.linspace(1, nb_repetitions, nb_fps)).astype(int), # nb_cones
-    # np.linspace(0, np.pi, nb_fps), # ring_tilt
-    # np.linspace(np.pi, np.pi / 2, nb_fps // 2), # ring_tilt
-    # [None] * (nb_fps // 4), # None
-    # Seiffert shells
-    # np.around(np.linspace(1, nb_repetitions, nb_fps)).astype(int), # nb_cones
-    # np.linspace(0, 0.95, nb_fps), # curve_index
-    # np.linspace(0.95, 0.5, nb_fps), # curve_index
-    # np.linspace(1, 3, nb_fps), # nb_revolutions
-    # np.linspace(3, 1, nb_fps // 2), # nb_revolutions
-    # [None] * (nb_fps // 4), # None
+    np.around(np.linspace(1, nb_repetitions, 4 * nb_frames)).astype(int),  # nb_cones
+    np.linspace(1, 3, 4 * nb_frames),  # spiral_reduction
+    [None] * nb_frames,  # None
     # Wave-CAIPI
-    np.linspace(0, 2, nb_fps),  # nb_revolutions & width
-    np.linspace(2, 1, nb_fps // 2),  # nb_revolutions & width
-    # np.around(np.linspace(0, 3, nb_fps)).astype(int), # packing
-    [None] * (nb_fps // 4),  # None
+    np.linspace(0, 2, 4 * nb_frames),  # nb_revolutions & width
+    np.linspace(2, 1, 2 * nb_frames),  # nb_revolutions & width
+    [None] * nb_frames,  # None
 ]
-
-# %%
 
 
 frame_setup = [
@@ -259,7 +175,6 @@ image_files = joblib.Parallel(n_jobs=1)(
 )
 
 
-# %%
 # Make a GIF of all images.
 imgs = [Image.open(img) for img in image_files]
 imgs[0].save(
@@ -267,7 +182,7 @@ imgs[0].save(
     save_all=True,
     append_images=imgs[1:],
     optimize=False,
-    duration=200,
+    duration=duration,
     loop=0,
 )
 
