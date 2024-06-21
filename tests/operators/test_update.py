62561709fd966da5e7d2195a10d8b5e340afbb87
Chaithya G R <chaithyagr@gmail.com>
2024-06-21 11:06:31 +0200

    Add test

--- tests/operators/test_update.py
+++ tests/operators/test_update.py
@@ -1,6 +1,6 @@
-"""Test for batch computations.
+"""Test for update in trajectory and density.
 
-Only finufft, cufinufft and gpunufft support batch computations.
+Only finufft, cufinufft and gpunufft support update.
 """
 
 import numpy as np
@@ -8,7 +8,6 @@ import numpy.testing as npt
 import pytest
 from pytest_cases import parametrize_with_cases, parametrize, fixture
 from helpers import (
-    assert_correlate,
     param_array_interface,
     to_interface,
     from_interface,
@@ -38,7 +37,7 @@ from case_trajectories import CasesTrajectories
     cases=CasesTrajectories,
     glob="*nyquist_radial*",
 )
-@parametrize(backend=["gpunufft", "finufft", "cufinufft"])
+@parametrize(backend=["gpunufft"])
 def operator(
     request,
     kspace_locs,
@@ -71,8 +70,7 @@ def operator(
     )
 
 
-@fixture(scope="module")
-def flat_operator(operator):
+def update_operator(operator):
     """Generate a batch operator with n_batch=1."""
     return get_operator(operator.backend)(
         operator.samples,
@@ -106,12 +104,16 @@ def kspace_data(operator):
 
 
 @param_array_interface
-def test_batch_op(operator, array_interface, flat_operator, image_data):
+def test_op(operator, array_interface, image_data):
     """Test the batch type 2 (forward)."""
     image_data = to_interface(image_data, array_interface)
 
+    gitter = np.random.rand(operator.samples.shape).astype(np.float32)
+    # Add very little noise to the trajectory, variance of 1e-3
+    operator.samples += gitter / 1000
+    new_operator = update_operator(operator)
+    
     kspace_batched = from_interface(operator.op(image_data), array_interface)
-
     if operator.uses_sense:
         image_flat = image_data.reshape(-1, *operator.shape)
     else:
@@ -119,7 +121,7 @@ def test_batch_op(operator, array_interface, flat_operator, image_data):
     kspace_flat = [None] * operator.n_batchs
     for i in range(len(kspace_flat)):
         kspace_flat[i] = from_interface(
-            flat_operator.op(image_flat[i]), array_interface
+            new_operator.op(image_flat[i]), array_interface
         )
 
     kspace_flat = np.reshape(
@@ -134,17 +136,26 @@ def test_batch_op(operator, array_interface, flat_operator, image_data):
 def test_batch_adj_op(
     operator,
     array_interface,
-    flat_operator,
     kspace_data,
 ):
     """Test the batch type 1 (adjoint)."""
     kspace_data = to_interface(kspace_data, array_interface)
-
+    
+    gitter = np.random.rand(operator.samples.shape).astype(np.float32)
+    # Add very little noise to the trajectory, variance of 1e-3
+    operator.samples += gitter / 1000
+    new_operator = update_operator(operator)
+    
     kspace_flat = kspace_data.reshape(-1, operator.n_coils, operator.n_samples)
     image_flat = [None] * operator.n_batchs
+    
+    gitter = np.random.rand(operator.samples.shape).astype(np.float32)
+    # Add very little noise to the trajectory, variance of 1e-3
+    operator.samples += gitter / 1000
+    
     for i in range(len(image_flat)):
         image_flat[i] = from_interface(
-            flat_operator.adj_op(kspace_flat[i]), array_interface
+            new_operator.adj_op(kspace_flat[i]), array_interface
         )
 
     if operator.uses_sense:
@@ -160,64 +171,3 @@ def test_batch_adj_op(
     image_batched = from_interface(operator.adj_op(kspace_data), array_interface)
     # Reduced accuracy for the GPU cases...
     npt.assert_allclose(image_batched, image_flat, atol=1e-3, rtol=1e-3)
-
-
-@param_array_interface
-def test_data_consistency(
-    operator,
-    array_interface,
-    image_data,
-    kspace_data,
-):
-    """Test the data consistency operation."""
-    # image_data = np.zeros_like(image_data)
-    image_data = to_interface(image_data, array_interface)
-    kspace_data = to_interface(kspace_data, array_interface)
-
-    res = operator.data_consistency(image_data, kspace_data)
-    tmp = operator.op(image_data)
-    res2 = operator.adj_op(tmp - kspace_data)
-
-    #    npt.assert_allclose(res.squeeze(), res2.squeeze(), atol=1e-4, rtol=1e-1)
-    res = res.reshape(-1, *operator.shape)
-    res2 = res2.reshape(-1, *operator.shape)
-
-    res = from_interface(res, array_interface)
-    res2 = from_interface(res2, array_interface)
-    slope_err = 1e-3
-    r_value_err = 1e-3
-    # FIXME 2D Sense is not very accurate...
-    if len(operator.shape) == 2 and operator.uses_sense:
-        print("Reduced accuracy for 2D Sense")
-        slope_err = 1e-1
-        r_value_err = 1e-1
-
-    for i in range(len(res)):
-        assert_correlate(res[i], res2[i], slope_err=slope_err, r_value_err=r_value_err)
-
-
-def test_data_consistency_readonly(operator, image_data, kspace_data):
-    """Test that the data consistency does not modify the input parameters data."""
-    kspace_tmp = kspace_data.copy()
-    image_tmp = image_data.copy()
-    kspace_tmp.setflags(write=False)
-    image_tmp.setflags(write=False)
-    operator.data_consistency(image_data, kspace_tmp)
-    npt.assert_equal(kspace_tmp, kspace_data)
-    npt.assert_equal(image_tmp, image_data)
-
-
-def test_gradient_lipschitz(operator, image_data, kspace_data):
-    """Test the gradient lipschitz constant."""
-    C = 1 if operator.uses_sense else operator.n_coils
-    img = image_data.copy().reshape(operator.n_batchs, C, *operator.shape).squeeze()
-    for _ in range(10):
-        grad = operator.data_consistency(img, kspace_data)
-        norm = np.linalg.norm(grad)
-        grad /= norm
-        np.copyto(img, grad.squeeze())
-        norm_prev = norm
-
-    # TODO: check that the value is "not too far" from 1
-    # TODO: to do the same with density compensation
-    assert (norm - norm_prev) / norm_prev < 1e-3
