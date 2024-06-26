24f3d1a71a48dfb4b8e05e18e5518079d01d3a6f
Chaithya G R <chaithyagr@gmail.com>
2024-06-26 09:49:39 +0200

    Fix the density_methodd

--- src/mrinufft/operators/base.py
+++ src/mrinufft/operators/base.py
@@ -360,7 +360,11 @@ class FourierOperatorBase(ABC):
             method = get_density(method)
         if not callable(method):
             raise ValueError(f"Unknown density method: {method}")
-        self.density_method = method
+        self.density_method = lambda samples, shape: method(
+            samples,
+            shape,
+            **kwargs,
+        )
         self.density = method(self.samples, self.shape, **kwargs)
 
     def get_lipschitz_cst(self, max_iter=10, **kwargs):
