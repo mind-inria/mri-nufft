a97be0511e569c0b9f6735719f2728829c5a119d
Chaithya G R <chaithyagr@gmail.com>
2024-09-16 10:52:07 +0200

    Add supoport for grads and slew

--- src/mrinufft/trajectories/display3D.py
+++ src/mrinufft/trajectories/display3D.py
@@ -1,4 +1,10 @@
 from mrinufft import get_operator, get_density
+from mrinufft.trajectories.utils import (
+    convert_trajectory_to_gradients,
+    convert_gradients_to_slew_rates,
+    KMAX,
+    DEFAULT_RASTER_TIME,
+)
 import numpy as np
 from typing import Tuple
 from mrinufft.io import read_trajectory
@@ -11,6 +17,7 @@ def get_gridded_trajectory(
     grid_type: str = "density",
     turbo_factor: int = 176,
     backend: str = "gpunufft",
+    traj_params: dict = None,
 ):
     """
     Compute the gridded trajectory for MRI reconstruction.
@@ -29,16 +36,21 @@ def get_gridded_trajectory(
         It can be one of the following:
             "density" : Get the sampling density in closest number of samples per voxel.
             Helps understand suboptimal sampling.
-            "time" : Get the sampling in time, this is helpful to view and understand 
+            "time" : Get the sampling in time, this is helpful to view and understand
             off-resonance effects.
-            "inversion" : Relative inversion time at the sampling location. Needs 
+            "inversion" : Relative inversion time at the sampling location. Needs
             turbo_factor to be set.
             "holes": Show the k-space holes within a elliosoid of the k-space.
+            "gradients": Show the gradient strengths of the k-space trajectory.
+            "slew": Show the slew rate of the k-space trajectory.
     turbo_factor : int, optional
         The turbo factor when sampling is with inversion. Default is 176.
     backend : str, optional
         The backend to use for gridding. Default is "gpunufft".
         Note that "gpunufft" is anyway used to get the `pipe` density internally.
+    traj_params : dict, optional
+        The trajectory parameters. Default is None.
+        This is only needed when `grid_type` is "gradients" or "slew".
 
     Returns
     -------
@@ -74,4 +86,23 @@ def get_gridded_trajectory(
                 np.meshgrid(*[np.linspace(-1, 1, sh) for sh in shape], indexing="ij")
             )
         ] = 0
-    return np.squeeze(np.abs(data) / np.abs(gridded_ones))
+    elif grid_type in ["gradients", "slew"]:
+        gradients, initial_position = convert_trajectory_to_gradients(
+            shots,
+            norm_factor=KMAX,
+            resolution=np.asarray(traj_params["FOV"])
+            / np.asarray(traj_params["img_size"]),
+            raster_time=DEFAULT_RASTER_TIME,
+            gamma=traj_params["gamma"],
+        )
+        if grid_type == "gradients":
+            data = np.hstack(
+                [gradients, np.zeros((gradients.shape[0], 1, gradients.shape[2]))]
+            )
+        else:
+            slews, _ = convert_gradients_to_slew_rates(gradients, DEFAULT_RASTER_TIME)
+            data = np.hstack([slews, np.zeros((slews.shape[0], 2, slews.shape[2]))])
+        data = grid_op.raw_op.adj_op(
+            np.linalg.norm(data, axis=-1).flatten(), None, True
+        )
+    return np.squeeze(np.abs(data))  # / np.abs(gridded_ones))
