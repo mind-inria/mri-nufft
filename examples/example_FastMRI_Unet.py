17314d07e80e1a96a0ddcb9c19eabada9df45519
Lena OUDJMAN <lo276838@is231077.intra.cea.fr>
2024-09-25 14:20:55 +0200

    fix ref

--- examples/example_FastMRI_Unet.py
+++ examples/example_FastMRI_Unet.py
@@ -21,21 +21,7 @@ the field of medical imaging using machine learning techniques.
 
 References
 ----------
-- FastMRI Facebook repository: https://github.com/facebookresearch/fastMRI
-@misc{zbontar2018fastMRI,
-    title={{fastMRI}: An Open Dataset and Benchmarks for Accelerated {MRI}},
-    author={Jure Zbontar and Florian Knoll and Anuroop Sriram and Tullie Murrell 
-        and Zhengnan Huang and Matthew J. Muckley and Aaron Defazio and Ruben Stern 
-        and Patricia Johnson and Mary Bruno and Marc Parente and Krzysztof J. Geras 
-        and Joe Katsnelson and Hersh Chandarana and Zizhao Zhang and Michal Drozdzal 
-        and Adriana Romero and Michael Rabbat and Pascal Vincent and Nafissa Yakubova 
-        and James Pinkerton and Duo Wang and Erich Owens and C. Lawrence Zitnick 
-        and Michael P. Recht and Daniel K. Sodickson and Yvonne W. Lui},
-    journal = {ArXiv e-prints},
-    archivePrefix = "arXiv",
-    eprint = {1811.08839},
-    year={2018}
-}
+- [source Unet] FastMRI Facebook: https://github.com/facebookresearch/fastMRI/blob/main/fastmri/models/unet.py
 
 .. warning::
     The training process is make on one image, so the training is not optimal.
