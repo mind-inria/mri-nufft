f9de809a7f77762b63866311b5b14104ac863188
femal.eprog <tichirrasoukainaa@gmail.com>
2025-04-18 14:54:08 +0200

    style : fix linter line too long (E501)

--- src/mrinufft/operators/off_resonance.py
+++ src/mrinufft/operators/off_resonance.py
@@ -199,7 +199,8 @@ class MRIFourierCorrected(FourierOperatorBase):
         The default is ``None`` (purely imaginary field).
         Also supports Cupy arrays and Torch tensors.
     isign : int, optional
-        Sign convention for spatial phase. Either `-1` (default, e^{-iωt}) or `1` (e^{iωt}).
+        Sign convention for spatial phase. 
+        Either `-1` (default, e^{-iωt}) or `1` (e^{iωt}).
     backend: str, optional
         The backend to use for computations. Either 'cpu', 'gpu' or 'torch'.
         The default is `cpu`.
@@ -269,7 +270,8 @@ class MRIFourierCorrected(FourierOperatorBase):
                 r2star_map,
             )
         if self.B is None or self.tl is None:
-            raise ValueError("Please either provide fieldmap and t or B and tl")
+            raise ValueError(
+                "Please either provide fieldmap and t or B and tl")
         self.n_interpolators = self.B.shape[0]
 
         # create spatial interpolator
@@ -278,7 +280,8 @@ class MRIFourierCorrected(FourierOperatorBase):
             self.C = None
             self.field_map = field_map
         else:
-            self.C = _get_spatial_coefficients(field_map, self.tl, isign=self.isign)
+            self.C = _get_spatial_coefficients(
+                field_map, self.tl, isign=self.isign)
             self.field_map = None
 
     def op(self, data, *args):
@@ -301,7 +304,8 @@ class MRIFourierCorrected(FourierOperatorBase):
         data_d = self.xp.asarray(data)
         if self.C is not None:
             for idx in range(self.n_interpolators):
-                y += self.B[idx] * self._fourier_op.op(self.C[idx] * data_d, *args)
+                y += self.B[idx] * \
+                    self._fourier_op.op(self.C[idx] * data_d, *args)
         else:
             for idx in range(self.n_interpolators):
                 C = self.xp.exp(-self.field_map * self.tl[idx].item())
