2cb0d2c86a7054e000f065a6075e332402c9fda9
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2025-10-09 11:30:09 +0200

    feat: add integration test for off-resonance correction

--- tests/helpers/asserts.py
+++ tests/helpers/asserts.py
@@ -54,6 +54,9 @@ def assert_correlate(a, b, slope=1.0, slope_err=1e-3, r_value_err=1e-3):
         a.flatten(), b.flatten()
     )
     abs_slope_reg = abs(slope_reg)
+    if np.iscomplex(rvalue):
+        rvalue = abs(rvalue)
+
     if r_value_err is not None and abs(rvalue - 1) > r_value_err:
         raise AssertionError(
             f"RValue {rvalue} != 1 ± {r_value_err}\n "
