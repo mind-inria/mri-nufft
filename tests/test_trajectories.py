42cedd46ae9ab5bb5bde5ff78b0692d9a1fc0ce2
chaithyagr <chaithyagr@gitlab.com>
2025-10-17 14:10:17 +0200

    Fix

--- tests/test_trajectories.py
+++ tests/test_trajectories.py
@@ -13,7 +13,7 @@ def test_wave_caipi_gradients(wavegrad=8.8e-3, caipi_delta=2, nb_revolutions=11)
     acq = Acquisition.default
     wavegrad = 8.8e-3
     traj = initialize_3D_wave_caipi(
-        (3, 3)
+        (3, 3),
         417,
         packing="square",
         shape="square",
