abbcaa496863bfb82417fab68bd2371a59317530
chaithyagr <chaithyagr@gitlab.com>
2025-11-04 09:38:16 +0100

    Added cartesian

---
+++ src/mrinufft/extras/cartesian.py
@@ -0,0 +1,29 @@
+from mrinufft._array_compat import get_array_module
+
+
+def fft(image, dim=3, shape=None):
+    axes = range(-dim, 0)
+    xp = get_array_module(image)
+    return xp.fft.fftshift(
+        xp.fft.fftn(
+            xp.fft.ifftshift(image, axes=axes),
+            norm="ortho",
+            axes=axes,
+            s=shape,
+        ),
+        axes=axes,
+    )
+
+
+def ifft(kspace, dim=3, shape=None):
+    axes = range(-dim, 0)
+    xp = get_array_module(kspace)
+    return xp.fft.fftshift(
+        xp.fft.ifftn(
+            xp.fft.ifftshift(kspace, axes=axes), 
+            norm="ortho",
+            axes=axes,
+            s=shape,
+        ),
+        axes=axes,
+    )
