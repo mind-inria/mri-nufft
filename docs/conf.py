caf9003af55e919aed045d66be3c26e88c4db70d
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2025-11-13 11:06:15 +0100

    feat: improve css styling.

--- docs/conf.py
+++ docs/conf.py
@@ -127,6 +127,11 @@ html_theme = "sphinx_book_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+html_css_files = [
+    "custom.css",
+]
+
 html_theme_options = {
     "repository_url": GITHUB_REPO,
     "use_repository_button": True,
