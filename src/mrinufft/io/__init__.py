999c40584f4c1192673c7f0d9ccb4db3700a5da5
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2025-11-19 10:59:07 +0100

    update pulseq

--- src/mrinufft/io/__init__.py
+++ src/mrinufft/io/__init__.py
@@ -3,13 +3,15 @@
 from .cfl import traj2cfl, cfl2traj
 from .nsp import read_trajectory, write_trajectory, read_arbgrad_rawdat
 from .siemens import read_siemens_rawdat
-
+from .pulseq import read_pulseq_traj, pulseq_gre
 
 __all__ = [
-    "traj2cfl",
     "cfl2traj",
-    "read_trajectory",
-    "write_trajectory",
+    "pulseq_gre",
     "read_arbgrad_rawdat",
+    "read_pulseq_traj",
     "read_siemens_rawdat",
+    "read_trajectory",
+    "traj2cfl",
+    "write_trajectory",
 ]
