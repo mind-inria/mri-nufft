03f1ad77e08a0879e76494f244d5ad21528ce9b7
mreineck <martin.reinecke1@gmx.de>
2026-01-17 17:10:46 +0100

    Add DUCC0 backend (#345)
    * first try
    
    * first try
    
    * try to make black happy
    
    * try to enable ducc0 backend tests
    
    * add some documentation
    
    * add kwargs
    
    * add docstring
    
    * simplify
    
    * pull out nthreads from kwargs, to allow manipulating the default
    
    * use all available threads by default

---
+++ src/mrinufft/operators/interfaces/ducc0.py
@@ -0,0 +1,80 @@
+"""An implementation of the NUDFT using numpy."""
+
+import numpy as np
+import os
+
+from ..base import FourierOperatorCPU
+
+DUCC0_AVAILABLE = True
+try:
+    import ducc0
+except ImportError:
+    DUCC0_AVAILABLE = False
+
+
+class RawDUCC0:
+    """Binding for the ducc0 package.
+
+    Parameters
+    ----------
+    samples: array
+        The samples location of shape ``Nsamples x N_dimensions``.
+        It should be C-contiguous.
+    shape: tuple
+        Shape of the image space.
+    eps: float, default=1e-6
+        Requested accuracy of the transform.
+        Useful values go from 1e-2 down to 1e-13 (for double precision).
+    nthreads: int, default=0
+        Number of threads to use for the transforms.
+        0 uses as many threads as there are available for the current process.
+        if negative, uses `(available threads) + 1 + nthreads` threads.
+    """
+
+    def __init__(self, samples, shape, eps=1e-6, nthreads=0, **kwargs):
+        self.samples = samples
+        self.shape = shape
+        if nthreads == 0:
+            nthreads = len(os.sched_getaffinity(0))
+        elif nthreads < 0:
+            nthreads = len(os.sched_getaffinity(0)) + 1 + nthreads
+        self.plan = ducc0.nufft.plan(
+            nu2u=False,  # can be used for both directions
+            coord=samples,
+            grid_shape=shape,
+            epsilon=eps,
+            nthreads=nthreads,
+            periodicity=1.0,  # must be 1, otherwise conventions don't match
+            fft_order=False,  # must be False, otherwise conventions don't match
+            **kwargs,
+        )
+
+    def op(self, coeffs, image):
+        """Compute the forward NUDFT."""
+        return self.plan.u2nu(forward=True, grid=image, out=coeffs)
+
+    def adj_op(self, coeffs, image):
+        """Compute the adjoint NUDFT."""
+        return self.plan.nu2u(forward=False, points=coeffs, out=image)
+
+
+class MRIDUCC0(FourierOperatorCPU):
+    """MRI operator using ducc0 backend."""
+
+    backend = "ducc0"
+    available = DUCC0_AVAILABLE
+
+    def __init__(
+        self, samples, shape, n_coils=1, n_batchs=1, smaps=None, density=False, **kwargs
+    ):
+        super().__init__(
+            samples,
+            shape,
+            n_coils=n_coils,
+            n_batchs=n_batchs,
+            n_trans=1,
+            smaps=smaps,
+            density=density,
+            raw_op=None,  # is set later, after normalizing samples.
+        )
+        self.raw_op = RawDUCC0(self.samples, shape, **kwargs)
