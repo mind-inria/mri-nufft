0218b7009c18b2961965545ac32cc893de09a34f
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2026-01-26 17:52:00 +0100

    wip: batch support for kernel application.

--- src/mrinufft/operators/toeplitz.py
+++ src/mrinufft/operators/toeplitz.py
@@ -192,6 +192,7 @@ def apply_toeplitz_kernel(
     image: NDArray,
     toeplitz_kernel: NDArray,
     padded_array: NDArray | None = None,
+    paired_batch: bool = False,
 ) -> NDArray:
     """Apply the 2D or 3D Toeplitz kernel to an image using FFT.
 
@@ -204,6 +205,9 @@ def apply_toeplitz_kernel(
     padded_array : NDArray | None, optional
         An optional pre-allocated array for padding the image. If None,
         a new array will be created. Default is None.
+    paired_batch : bool, optional
+        If True, pairs the batch dimension of the image with the toeplitz_kernel.
+        Default is False, ie the same kernel is applied to all images in the batch.
 
     Returns
     -------
@@ -216,16 +220,32 @@ def apply_toeplitz_kernel(
     """
     xp = get_array_module(image)
     fftn, ifftn = _get_fftn(xp)
+    img_shape = image.shape
+    if image.ndim == toeplitz_kernel.ndim:
+        # add extra batch dimension
+        image = image[None, :]
+        batch_size = 1
+    elif image.ndim + 1 == toeplitz_kernel.ndim:
+        batch_size = image.shape[0]
+    else:
+        raise ValueError("Image and toeplitz_kernel must have compatible dimensions.")
+
     if padded_array is None:
-        padded_array = xp.zeros(toeplitz_kernel.shape, dtype=image.dtype)
+        padded_array = xp.zeros((batch_size, *toeplitz_kernel.shape), dtype=image.dtype)
+    elif batch_size == 1 and padded_array.ndim == toeplitz_kernel.ndim:
+        # expand padded_array to have batch dimension
+        padded_array = padded_array[None, :]
+
     elif padded_array.shape != toeplitz_kernel.shape:
         raise ValueError("padded_array shape must match toeplitz_kernel shape.")
 
-    tl_corner = tuple(slice(0, s) for s in image.shape)
+    tl_corner = (slice(None),) + tuple(slice(0, s) for s in image.shape)
 
+    # FIXME cannot unpack slice directly in python 3.10
     padded_array[tl_corner] = image
-    tmp = fftn(padded_array)
+    axis = tuple(range(1, padded_array.ndim))
+    tmp = fftn(padded_array, axis=axis)
     tmp *= toeplitz_kernel
-    result = ifftn(tmp, overwrite_x=True)
+    result = ifftn(tmp, overwrite_x=True, axis=axis)
 
-    return result[tl_corner]
+    return result[tl_corner].reshape(img_shape)
