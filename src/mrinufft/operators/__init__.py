39908a48cb4ebc969ad1c9f3c27ca1b8ef06d9f6
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2026-06-28 17:17:03 +0200

    refactor: lazy loading of array modules.

--- src/mrinufft/operators/__init__.py
+++ src/mrinufft/operators/__init__.py
@@ -79,7 +79,3 @@ def __getattr__(name):
 
         return getattr(autodiff, name)
     raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
-        "DeepInvPhyNufft",
-    ]
-except ImportError:
-    pass
